4e0070a5dbd5a37540fd93d1b08b6341328e36d9
jeromerousselot <jeromerousselot@users.sourceforge.net>
2006-10-27 08:52:29 +0000

    refactoring, bugfixes and features enhancements.

--- src/gsn/utils/protocols/ProtocolManager.java
+++ src/gsn/utils/protocols/ProtocolManager.java
@@ -3,6 +3,7 @@
  */
 package gsn.utils.protocols;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -33,88 +34,118 @@ import org.apache.log4j.Logger;
  *  @see AbstractHCIProtocol
  */
 public class ProtocolManager {
-   private static final transient Logger logger = Logger.getLogger( ProtocolManager.class );
-   private AbstractHCIProtocol protocol;
-   private ProtocolStates currentState;
-   private AbstractHCIQuery lastExecutedQuery = null;
-   private Vector < Object > lastParams;
-   
-   private Timer timer;
-   private TimerTask answerTimeout = new TimerTask() {
-      
-      public synchronized void run ( ) {
-         lastExecutedQuery = null;
-         currentState = ProtocolStates.READY;
-      } 
-   };
-   
-   public enum ProtocolStates {
-      READY, WAITING
-   }
-   
-   public ProtocolManager(AbstractHCIProtocol protocol) {
-      this.protocol = protocol;
-      currentState = ProtocolStates.READY;
-   }
-   
-   public synchronized ProtocolStates getCurrentState() {
-      return currentState;
-   }
-   
-   /*
-    * This method tries to execute a query named queryName with parameters params
-    * on the wrapper wrapper.
-    * If successful, it returns true.
-    */
-   public synchronized boolean sendQuery(String queryName, Vector<Object> params, StreamProducer wrapper) {
-      boolean successful = false;
-      if(currentState == ProtocolStates.READY) {
-         AbstractHCIQuery query = protocol.getQuery( queryName );
-         if(logger.isDebugEnabled())
-            logger.debug( "Retrieved query " + queryName + ", trying to build raw query.");
-         if(query != null) {
-            byte[] queryBytes = query.buildRawQuery( params );
-            if(queryBytes != null) {
-               try {
-                  if(logger.isDebugEnabled())
-                     logger.debug("Built query, it looks like: " + new String(queryBytes));
-                  wrapper.sendToWrapper(queryBytes);
-                  lastExecutedQuery = query;
-                  lastParams = params;
-                  successful = true;
-                  if(logger.isDebugEnabled())
-                     logger.debug("Query succesfully sent!");
-                  if(query.needsAnswer( params )) {
-                     if(logger.isDebugEnabled())
-                        logger.debug("Now entering wait mode for answer.");
-                     timer = new Timer();
-                     currentState = ProtocolStates.WAITING;
-                     timer.schedule( answerTimeout , new Date());
-                  }
-               } catch( OperationNotSupportedException e ) {
-                  if(logger.isDebugEnabled())
-                     logger.debug("Query could not be sent ! See error message.");
-                  logger.error( e.getMessage( ) , e );
-                  currentState = ProtocolStates.READY;
-               }            
-            }
-         }
-      }
-      return successful;
-   }
-   
-   /*
-    * This tries to match incoming data to the pattern
-    * expected by the query. If the pattern describes
-    * several groups then all the different String
-    * matching these groups are returned.
-    */
-   public synchronized Object[] getAnswer(byte[] rawData) {
-      Object[] answer = null;
-      if(currentState == ProtocolStates.WAITING) {
-    	  answer = lastExecutedQuery.getAnswers(rawData);
-      }
-      return answer;
-   }
+	private static final transient Logger logger = Logger.getLogger( ProtocolManager.class );
+	private AbstractHCIProtocol protocol;
+	private ProtocolStates currentState;
+	private AbstractHCIQuery lastExecutedQuery = null;
+	private Vector < Object > lastParams;
+	private StreamProducer outputWrapper;
+
+	private Timer timer;
+	private TimerTask answerTimeout = new TimerTask() {
+
+		public synchronized void run ( ) {
+			lastExecutedQuery = null;
+			currentState = ProtocolStates.READY;
+		} 
+	};
+
+	public enum ProtocolStates {
+		READY, WAITING
+	}
+
+	public ProtocolManager(AbstractHCIProtocol protocol, StreamProducer outputWrapper) {
+		this.protocol = protocol;
+		this.outputWrapper = outputWrapper;
+		currentState = ProtocolStates.READY;
+	}
+
+	public synchronized ProtocolStates getCurrentState() {
+		return currentState;
+	}
+
+	/*
+	 * This method tries to execute a query named queryName with parameters params
+	 * on the wrapper wrapper.
+	 * If successful, it returns the raw command that has been sent.
+	 */
+	public synchronized byte[] sendQuery(String queryName, Vector<Object> params) {
+		byte[] answer = null;
+		if(currentState == ProtocolStates.READY) {
+			AbstractHCIQuery query = protocol.getQuery( queryName );
+			if(logger.isDebugEnabled())
+				logger.debug( "Retrieved query " + queryName + ", trying to build raw query.");
+			if(query != null) {
+				byte[] queryBytes = query.buildRawQuery( params );
+				if(queryBytes != null) {
+					try {
+						if(logger.isDebugEnabled())
+							logger.debug("Built query, it looks like: " + new String(queryBytes));
+						outputWrapper.sendToWrapper(queryBytes);
+						lastExecutedQuery = query;
+						lastParams = params;
+						answer = queryBytes;
+						if(logger.isDebugEnabled())
+							logger.debug("Query succesfully sent!");
+						if(query.needsAnswer( params )) {
+							if(logger.isDebugEnabled())
+								logger.debug("Now entering wait mode for answer.");
+							timer = new Timer();
+							currentState = ProtocolStates.WAITING;
+							timer.schedule( answerTimeout , new Date());
+						}
+					} catch( OperationNotSupportedException e ) {
+						if(logger.isDebugEnabled())
+							logger.debug("Query could not be sent ! See error message.");
+						logger.error( e.getMessage( ) , e );
+						currentState = ProtocolStates.READY;
+					}            
+				}
+			}
+		}
+		return answer;
+	}
+
+	/*
+	 * This tries to match incoming data to the pattern
+	 * expected by the query. If the pattern describes
+	 * several groups then all the different String
+	 * matching these groups are returned.
+	 */
+	public synchronized Object[] getAnswer(byte[] rawData) {
+		Object[] answer = null;
+		if(currentState == ProtocolStates.WAITING) {
+			answer = lastExecutedQuery.getAnswers(rawData);
+		}
+		return answer;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getProtocolName() {
+		if(protocol != null)
+			return protocol.getName();
+		return null;
+	}
+
+	/**
+	 * @param string
+	 * @return
+	 */
+	public AbstractHCIQuery getQuery(String string) {
+		if(protocol != null)
+			return protocol.getQuery(string);
+		return null;
+	}
+
+	/**
+	 * @return
+	 */
+	public Collection<AbstractHCIQuery> getQueries() {
+		if(protocol != null)
+			return protocol.getQueries();
+		return null;
+	}
 }
 
