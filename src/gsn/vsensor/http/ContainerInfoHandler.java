c7ba15420a93880905a69cddc5005cff1d449524
ali_salehi <salehiam@gmail.com>
2006-11-01 15:27:09 +0000

    Compressing the response.

--- src/gsn/vsensor/http/ContainerInfoHandler.java
+++ src/gsn/vsensor/http/ContainerInfoHandler.java
@@ -19,13 +19,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.collections.KeyValue;
 import org.apache.commons.lang.StringEscapeUtils;
 
-
 /**
  * @author alisalehi
- *
  */
-public class ContainerInfoHandler implements RequestHandler{
-
+public class ContainerInfoHandler implements RequestHandler {
+   
    public void handle ( HttpServletRequest request , HttpServletResponse response ) throws IOException {
       response.setStatus( HttpServletResponse.SC_OK );
       Iterator < VSensorConfig > vsIterator = Mappings.getAllVSensorConfigs( );
@@ -36,33 +34,33 @@ public class ContainerInfoHandler implements RequestHandler{
       sb.append( "description=\"" ).append( StringEscapeUtils.escapeXml( Main.getContainerConfig( ).getWebDescription( ) ) ).append( "\" >\n" );
       while ( vsIterator.hasNext( ) ) {
          VSensorConfig sensorConfig = vsIterator.next( );
-         sb.append( "<virtual-sensor name=\"" ).append( sensorConfig.getVirtualSensorName( ) ).append( "\"").append( " last-modified=\"" ).append( new File( sensorConfig.getFileName( ) ).lastModified( ) ).append( "\"").append( " >\n" );
-         for ( DataField df : sensorConfig.getOutputStructure( ) )
-            sb.append( "<field name=\"" ).append( df.getFieldName( ) ).append( "\" " ).append( "type=\"" ).append( df.getType( ) ).append( "\" " ).append( "description=\"" ).append(
-               StringEscapeUtils.escapeXml( df.getDescription( ) ) ).append( "\" />\n" );
-         sb.append( "<field name=\"TIMED\" type=\"long\" description=\"The timestamp associated with the stream element\" />\n" );
-         for ( KeyValue df : sensorConfig.getAddressing( ) )
-            sb.append( "<predicate key=\"" ).append( StringEscapeUtils.escapeXml( df.getKey( ).toString( ) ) ).append( "\">" ).append( StringEscapeUtils.escapeXml( df.getValue( ).toString( ) ) ).append( "</predicate>\n" );
-         StringBuilder query = new StringBuilder( "select * from " + sensorConfig.getVirtualSensorName( ) +" order by TIMED limit 1 offset 0" );
+         sb.append( "<virtual-sensor name=\"" ).append( sensorConfig.getVirtualSensorName( ) ).append( "\"" ).append( " last-modified=\"" ).append(
+            new File( sensorConfig.getFileName( ) ).lastModified( ) ).append( "\"" ).append( " >\n" );
+         StringBuilder query = new StringBuilder( "select * from " + sensorConfig.getVirtualSensorName( ) + " order by TIMED limit 1 offset 0" );
          Enumeration < StreamElement > result = StorageManager.getInstance( ).executeQuery( query , true );
-         while ( result.hasMoreElements( ) ) {
-            StreamElement se = result.nextElement( );
-            sb.append( "<stream-element>\n" );
-            for ( int i = 0 ; i < se.getFieldNames( ).length ; i++ )
-               if ( se.getFieldTypes( )[ i ] == DataTypes.BINARY )
-                  sb.append( "<field name=\"" ).append( se.getFieldNames( )[ i ] ).append( "\">" ).append( se.getData( )[ i ].toString( ) ).append( "</field>\n" );
-               else
-                  sb.append( "<field name=\"" ).append( se.getFieldNames( )[ i ] ).append( "\">" ).append( StringEscapeUtils.escapeXml( se.getData( )[ i ].toString( ) ) ).append( "</field>\n" );
-            sb.append( "<field name=\"TIMED\" >" ).append( se.getTimeStamp( ) ).append( "</field>\n" );
-            sb.append( "</stream-element>\n" );
+         StreamElement se = null;
+         if ( result.hasMoreElements( ) ) se = result.nextElement( );
+         for ( DataField df : sensorConfig.getOutputStructure( ) ) {
+            sb.append( "<field name=\"" ).append( df.getFieldName( ) ).append( "\" " ).append( "type=\"" ).append( df.getType( ) ).append( "\" " );
+            if ( df.getDescription( ) != null && df.getDescription( ).trim( ).length( ) != 0 ) sb.append( "description=\"" ).append( StringEscapeUtils.escapeXml( df.getDescription( ) ) ).append( "\"" );
+            sb.append( " >" );
+            if ( se != null ) if ( df.getType( ).toLowerCase( ).trim( ).indexOf( "binary" ) > 0 )
+               sb.append( se.getData( df.getFieldName( ) ) );
+            else
+               sb.append( se.getData( StringEscapeUtils.escapeXml( df.getFieldName( ) ) ) );
+            sb.append( "</field>" );
          }
+         sb.append( "<field name=\"TIMED\" type=\"long\" description=\"The timestamp associated with the stream element\" >" ).append( se == null ? "" : se.getTimeStamp( ) ).append( "</field>\n" );
+         for ( KeyValue df : sensorConfig.getAddressing( ) )
+            sb.append( "<field name=\"" ).append( StringEscapeUtils.escapeXml( df.getKey( ).toString( ) ) ).append( "\" type=\"predicate\" >" ).append(
+               StringEscapeUtils.escapeXml( df.getValue( ).toString( ) ) ).append( "</field>\n" );
          sb.append( "</virtual-sensor>\n" );
       }
       sb.append( "</gsn>\n" );
       response.getWriter( ).write( sb.toString( ) );
       
    }
-
+   
    public boolean isValid ( HttpServletRequest request , HttpServletResponse response ) throws IOException {
       return true;
    }
