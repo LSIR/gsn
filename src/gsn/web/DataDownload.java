60d820822ec2d237ebd0d389d086adfe786959c5
parobert <parobert@users.sourceforge.net>
2006-11-26 12:59:24 +0000

    Added many functionalities to the download of datas (like XML/CVS format)

--- src/gsn/web/DataDownload.java
+++ src/gsn/web/DataDownload.java
@@ -17,6 +17,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.log4j.Logger;
@@ -36,12 +38,19 @@ public class DataDownload extends HttpServlet {
 	}
 	
 	public void doPost ( HttpServletRequest req , HttpServletResponse res ) throws ServletException , IOException {
-	      PrintWriter out = res.getWriter();
+	      boolean responseCVS = false;
+	      boolean wantTimeStamp = false;
+		  PrintWriter out = res.getWriter();
 	      if (req.getParameter("vsName")==null || req.getParameter("vsName").equals("")) {
 	    	  res.sendError( Container.MISSING_VSNAME_ERROR , "The virtual sensor name is missing" );
 	    	  return;
 	      }
-	      res.setContentType("text/csv");
+	      if (req.getParameter("display") != null && req.getParameter("display").equals("CSV")) {
+	    	  responseCVS = true;
+		      res.setContentType("text/csv");
+	      } else {
+	    	  res.setContentType("text/xml");
+	      }
 	      String vsName = req.getParameter("vsName");
 	      String delimiter = ";";
 	      if (req.getParameter("delimiter") != null && !req.getParameter("delimiter").equals("")) {
@@ -59,11 +68,12 @@ public class DataDownload extends HttpServlet {
 	      if (req.getParameter("fields") != null) {
 	    	  String[] fields = req.getParameterValues("fields");
 		      for (int i=0; i < fields.length; i++) {
+		    	  if (fields[i].equals("TIMED")) {
+		    		  wantTimeStamp = true;
+		    	  }
 		    	  request += ", " + fields[i];
-		    	  line += delimiter + fields[i];
 		      }    
 	      }
-	      out.println(line.substring(delimiter.length()));
 	      
 	      String limit = "";
 	      	      if (req.getParameter("nb") != null && req.getParameter("nb") != "") {
@@ -74,32 +84,116 @@ public class DataDownload extends HttpServlet {
 	      }
 	      String where = "";
 	      if (req.getParameter("critfield") != null) {
+	    	  String[] critJoin = req.getParameterValues("critJoin");
+	    	  String[] neg = req.getParameterValues("neg");
 	    	  String[] critfields = req.getParameterValues("critfield");
 	    	  String[] critop = req.getParameterValues("critop");
 	    	  String[] critval = req.getParameterValues("critval");
 	    	  for (int i=0; i < critfields.length ; i++) {
 	    		  if (critop[i].equals("LIKE")) {
-	    			  where += " AND " + critfields[i] + " LIKE '%" + critval[i] + "%'";
+	    			  if (i > 0) {
+	    				  where += " " + critJoin[i-1] + " " + neg[i] + " " + critfields[i] + " LIKE '%"; // + critval[i] + "%'";
+	    			  } else {
+	    				  where += neg[i] + " " + critfields[i] + " LIKE '%"; // + critval[i] + "%'";
+	    			  }
+	    			  if (critfields[i].equals("TIMED")) {
+	    				  try {
+	    					  SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy HH:mm:ss");
+	    					  Date d = sdf.parse(critval[i]);
+		    				  where += d.getTime();
+	    				  } catch (Exception e) {
+	    					  where += "0";
+	    				  }
+	    			  } else {
+	    				  where += critval[i];
+	    			  }
+	    			  where += "%'";
 	    		  } else {
-	    			  where += " AND " + critfields[i] + " " + critop[i] + " " + critval[i];
+	    			  if (i > 0) {
+	    				  where += " " + critJoin[i-1] + " " + neg[i] + " " + critfields[i] + " " + critop[i] + " "; //critval[i];
+	    			  } else {
+	    				  where += neg[i] + " " + critfields[i] + " " + critop[i] + " "; //critval[i];
+	    			  }
+	    			  if (critfields[i].equals("TIMED")) {
+	    				  try {
+	    					  SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy HH:mm:ss");
+	    					  Date d = sdf.parse(critval[i]);
+	    					  //System.out.println(d.getTime());
+		    				  where += d.getTime();
+	    				  } catch (Exception e) {
+	    					  System.out.println(e.toString());
+	    					  where += "0";
+	    				  }
+	    			  } else {
+	    				  where += critval[i];
+	    			  }
 	    		  }
 	    	  }
-	    	  where = where.substring(4);
+	    	  //where = where.substring(4);
 	    	  where = " WHERE " + where;
 	      }
 	      	      
 	      if (! request.equals("")) {
 	    	  request = request.substring(2);
 	    	  StringBuilder query = new StringBuilder("select "+request+" from " + vsName + where + " order by TIMED DESC "+limit+";");
+	    	  //out.println(query);
+	    	  ///*
 	    	  DataEnumerator  result = StorageManager.getInstance( ).executeQuery( query , false );
-	          while ( result.hasMoreElements( ) ) {
-	             StreamElement se = result.nextElement( );
-	             line = "";
-	             for ( int i = 0 ; i < se.getFieldNames( ).length ; i++ )
-	                   line += delimiter+se.getData( )[ i ].toString( );
-	             out.println(line.substring(delimiter.length()));
-	          }
+	    	  line = "";
+	    	  if (responseCVS) {
+	    		  boolean firstLine = true;
+	    		  out.println(query);
+		    	  //out.println(line.substring(delimiter.length()));
+		          while ( result.hasMoreElements( ) ) {
+		             StreamElement se = result.nextElement( );
+		             if (firstLine) {
+		            	 for (int i=0; i < se.getFieldNames().length; i++)
+		            		 line += delimiter + se.getFieldNames()[i].toString();
+		            	if (wantTimeStamp) {
+		            		line += delimiter + "TIMED";
+		            	}
+		             }
+		             out.println(line.substring(delimiter.length()));
+		             line = "";
+		             for ( int i = 0 ; i < se.getFieldNames( ).length ; i++ )
+		                   line += delimiter+se.getData( )[ i ].toString( );
+		             if (wantTimeStamp) {
+		            	SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy HH:mm:ss");
+					 	Date d = new Date (se.getTimeStamp());
+					 	line += delimiter + sdf.format(d);
+		             }
+		             out.println(line.substring(delimiter.length()));
+		          }
+	    	  } else {
+	    		  boolean firstLine = true;
+	    		  out.println("<data>");
+	    		  while ( result.hasMoreElements( ) ) {
+		             StreamElement se = result.nextElement( );
+	    			  if (firstLine) {
+	    				 out.println("\t<line>");
+	    				 for (int i = 0; i < se.getFieldNames().length; i++)
+	    					 out.println("\t\t<field>" + se.getFieldNames()[i].toString()+"</field>");
+	    				 if (wantTimeStamp) {
+	    					 out.println("\t\t<field>TIMED</field>");
+	    				 }
+	    				 out.println("\t</line>");
+	    				 firstLine = false;
+	    			  }
+		             line = "";
+		             out.println("\t<line>");
+		             for ( int i = 0 ; i < se.getFieldNames( ).length ; i++ )
+		                   out.println("\t\t<field>"+se.getData( )[ i ].toString( )+"</field>");
+		             if (wantTimeStamp) {
+		            	SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy HH:mm:ss");
+					 	Date d = new Date (se.getTimeStamp());
+		            	out.println("\t\t<field>"+sdf.format(d)+"</field>");
+		             }
+		             out.println("\t</line>");
+			       }
+	    		  out.println("</data>");
+	    	  }
 	          result.close();
+	          //*/
 	      } else {
 	    	  out.println("Please select some fields");
 	      }
