a33513c4f70be9c48636a04dd57fc195d652c561
mehdiriahi <mehdi.riahi@epfl.ch>
2007-03-15 18:56:26 +0000

    Implementing dependency graph : virtual sensors should be loaded in correct order

--- src/gsn/beans/Modifications.java
+++ src/gsn/beans/Modifications.java
@@ -1,9 +1,16 @@
 package gsn.beans;
 
+import gsn.Mappings;
+import gsn.utils.graph.Graph;
+import gsn.utils.graph.NodeNotExistsExeption;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
 import org.apache.log4j.Logger;
 import org.jibx.runtime.BindingDirectory;
 import org.jibx.runtime.IBindingFactory;
@@ -22,6 +29,8 @@ public final class Modifications {
    private ArrayList < VSensorConfig > addVirtualSensorConf    = new ArrayList < VSensorConfig >( );
    
    private ArrayList < VSensorConfig > removeVirtualSensorConf = new ArrayList < VSensorConfig >( );
+
+   private Graph<VSensorConfig> graph;
    
    private static transient Logger     logger                  = Logger.getLogger( Modifications.class );
    
@@ -31,6 +40,7 @@ public final class Modifications {
     * @return Returns the add.
     */
    public ArrayList < VSensorConfig > getAdd ( ) {
+	   fillGraph(addVirtualSensorConf.iterator());
       return addVirtualSensorConf;
    }
    
@@ -70,6 +80,7 @@ public final class Modifications {
                logger.error( new StringBuilder( ).append( "Please check the file and try again" ).toString( ) );
                continue ;
             }
+            
             list.add( configuration );
          } catch ( JiBXException e ) {
             logger.error( e.getMessage( ) , e );
@@ -96,6 +107,15 @@ public final class Modifications {
     */
    public void setRemove ( final Collection < String > listOfTheRemovedVirtualSensorsFileName ) {
       removeVirtualSensorConf.clear( );
+      for (String fileName : listOfTheRemovedVirtualSensorsFileName) {
+    	  VSensorConfig config = Mappings.getConfigurationObject(fileName);
+    	  try {
+			graph.removeNode(config);
+		} catch (NodeNotExistsExeption e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
       loadVirtualSensors( listOfTheRemovedVirtualSensorsFileName , removeVirtualSensorConf );
    }
    
@@ -106,11 +126,18 @@ public final class Modifications {
     * @param remove : The list of the virtual sensor descriptor files removed.
     */
    public Modifications ( final Collection < String > add , final Collection < String > remove ) {
+	  buildDependencyGraph(); 
       setAdd( add );
       setRemove( remove );
    }
    
-   private Collection < VSensorConfig > getModifications ( ) {
+   
+   
+   public Graph<VSensorConfig> getGraph() {
+	  return graph;
+   }
+
+private Collection < VSensorConfig > getModifications ( ) {
       // Finding the virtual sensors by nme
       // adapting the output structure.
       // making sure that the registered client's will not leave.
@@ -119,8 +146,45 @@ public final class Modifications {
    }
    
    private void buildDependencyGraph ( ) {
-      
+	   graph = new Graph<VSensorConfig>();
+	   Iterator<VSensorConfig> allVSensorConfigs = Mappings.getAllVSensorConfigs();
+	   fillGraph(allVSensorConfigs);
 
+
+	   if(logger.isDebugEnabled())
+		   logger.debug(graph.toString());
+
+
+   }
+
+   private void fillGraph(Iterator<VSensorConfig> allVSensorConfigs) {
+	   HashMap<String, VSensorConfig> vsNameTOVSConfig = new HashMap<String, VSensorConfig>();
+	   while(allVSensorConfigs.hasNext()){
+		   VSensorConfig config = allVSensorConfigs.next();
+		   vsNameTOVSConfig.put(config.getName().toLowerCase(), config);
+		   graph.addNode(config);
+	   }
+
+	   for(VSensorConfig config : vsNameTOVSConfig.values()){ 		
+		   Collection<InputStream> inputStreams = config.getInputStreams();
+		   for (InputStream stream : inputStreams) {
+			   StreamSource[] sources = stream.getSources();
+			   for (int i = 0; i < sources.length; i++) {
+				   AddressBean[] addressing = sources[i].getAddressing();
+				   for (int j = 0; j < addressing.length; j++) {
+					   String vsensorName = addressing[i].getPredicateValue("NAME");
+					   if (vsensorName != null) {
+						   VSensorConfig sensorConfig = vsNameTOVSConfig.get(vsensorName.toLowerCase());
+						   try {
+							   graph.addEdge(config, sensorConfig);
+						   } catch (NodeNotExistsExeption e) {
+							   e.printStackTrace();
+						   }
+					   }
+				   }
+			   }
+		   }
+	   }
    }
    
 }
