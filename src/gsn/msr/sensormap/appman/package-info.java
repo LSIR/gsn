2435591343883439eaa155c2ada362773dc9ddc2
ali_salehi <salehiam@gmail.com>
2008-02-19 18:40:00 +0000

    Update of MSR SensorMap integration. Still Alpha.

--- src/gsn/msr/sensormap/appman/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://nec.research.microsoft.com/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gsn.msr.sensormap.appman;
