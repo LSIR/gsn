6fffefcfa9b5672db275ad78d6f2042217594244
tmaret <timothee.maret@gmail.com>
2008-04-25 14:46:05 +0000

    Added the Apache Math library 'commons-math-1.2.jar' in the 'lib' folder in order to perform extended mathematical functions. This library documentation and API is available on the web ste: http://commons.apache.org/math/apidocs/index.html
    Finished the implementation of the window aware virtual sensors. A configuration file example of such a sensor is available in the 'ss_slf_fft_vs.xml' file.

--- src/ch/slf/FFTSample.java
+++ src/ch/slf/FFTSample.java
@@ -2,38 +2,64 @@ package ch.slf;
 
 import org.apache.log4j.Logger;
 
-import gsn.VSensorLoader;
-import gsn.beans.AddressBean;
-
+/**
+ * <p>
+ * Use the Common Math library developed by Apache in order to do extended
+ * mathematical and statistical computations.<br />
+ * The API is available on the web: <a href="http://commons.apache.org/math/apidocs/index.html">Apache Math Library</a>
+ * </p>
+ */
 public class FFTSample extends WindowAwareVS {
-  
-  private static transient Logger logger  = Logger.getLogger ( FFTSample.class );
-  
-  double fft_size;
-  double overlap ;
-  
-  public boolean initialize(AddressBean params) {
-    try {
-      fft_size= params.getPredicateValueAsIntWithException("window-size");
-      overlap = (params.getPredicateValueAsIntWithException("step-size") / fft_size) * 100.0;
-    }catch (Exception e) {
-      return false;
-    }
-    return true;
-  }
-  
-  
-  /**
-   * Output : One timestamp, set of double values for every input set, df .
-   */
-  public FFTOutputPacket process(double [] values , long[] timestampsInMSec) {
-    double sampling_rate = 1/((timestampsInMSec[timestampsInMSec.length-1] - timestampsInMSec[0])/timestampsInMSec.length)*1000;
-    // External call...
-    double[] results = null ;
-    
-    FFTOutputPacket out = new FFTOutputPacket(timestampsInMSec[timestampsInMSec.length/2]);
-    out.setdf(sampling_rate/values.length);
-    out.addValues(results);
-    return out;
-  }
+
+	private static transient Logger logger  = Logger.getLogger ( FFTSample.class );
+
+	double fft_size;
+
+	double overlap ;
+
+	public boolean init() {
+		try {
+			fft_size = getPredicateValueAsIntWithException("window-size");
+			overlap  = (getPredicateValueAsIntWithException("step-size") / fft_size) * 100.0;
+		}catch (Exception e) {
+			logger.error(e.getMessage(),e);
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @param values A two dimensional array that contains in first dimension (values[])
+	 *        the differents steps, and in the second dimension (value[step][]) the different
+	 *        measures for this step.
+	 * @param timestamps An array that contains the timestamps in milli seconds at every
+	 *        step.
+	 * @return
+	 */
+	public FFTOutputPacket process(double [][] values , long[] timestampsInMSec) {
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("FFTSample process method called with " + values.length + " steps of " + values[0].length + " measures");
+			// Print out the Datas to process
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0 ; i < values.length ; i++) {
+				sb.append("\n\n" + timestampsInMSec[i] + " | ");
+				for (int j = 0 ; j < values[i].length ; j++) {
+					sb.append(values[i][j] + " ");
+				}
+				sb.append("\n");
+			}
+			sb.append("\n");
+			logger.debug(sb.toString());
+		}
+		
+		//double sampling_rate = 1/((double)(timestampsInMSec[timestampsInMSec.length-1] - timestampsInMSec[0])/timestampsInMSec.length)*1000;
+		
+		FFTOutputPacket out = new FFTOutputPacket(timestampsInMSec[timestampsInMSec.length/2]);
+		double[] results = new double [] {1,2,3,0.0, 4, 5, 6} ;
+		out.setdf(1.2);
+		out.addValues(results);
+
+		return out;
+	}
 }
--- src/ch/slf/WindowAwareVS.java
+++ src/ch/slf/WindowAwareVS.java
@@ -1,28 +1,143 @@
 package ch.slf;
 
-import gsn.beans.AddressBean;
+import gsn.beans.DataField;
+import gsn.beans.DataTypes;
 import gsn.beans.StreamElement;
 import gsn.vsensor.AbstractVirtualSensor;
-import java.util.ArrayList;
 
+import java.io.Serializable;
+import java.util.Hashtable;
+import java.util.TreeMap;
+import java.util.concurrent.ArrayBlockingQueue;
 import org.apache.log4j.Logger;
 
 public abstract class WindowAwareVS extends AbstractVirtualSensor {
 
-  private static transient Logger                                logger                              = Logger.getLogger ( WindowAwareVS.class );
-  
-  public void dataAvailable(String inputStreamName, StreamElement streamElement) {
-    
-  }
+	private static final String WINDOW_SIZE = "window-size";
 
-  public void finalize() {
-    
-  }
+	private static final String STEP_SIZE = "step-size";
+	
+	private static final String TIMESTAMP = "TIMESTAMP";
+	
+	private static final String DF = "DF";
+	
+	private static final String VALUES = "VALS";
+	
+	private static final DataField [] outputStructure = new DataField[] { 
+		new DataField(TIMESTAMP, DataTypes.BIGINT_NAME), 
+		new DataField(DF, DataTypes.DOUBLE_NAME), 
+		new DataField(VALUES, "BLOB")
+	};
 
-  public boolean initialize() {
-    return false;
-  }
-  
-  public abstract FFTOutputPacket process(double [] values , long[] timestamps);
-  public abstract boolean initialize(AddressBean params);
+	private static transient Logger logger = Logger.getLogger ( WindowAwareVS.class );
+
+	private Hashtable<String, ArrayBlockingQueue<StreamElement>> circularBuffers;
+
+	private int windowSize = -1;
+
+	private int stepSize = -1;
+
+	public boolean initialize() {	
+		circularBuffers = new Hashtable<String, ArrayBlockingQueue<StreamElement>> () ;
+		// Get the parameters from the XML configuration file
+		TreeMap <  String , String > params = getVirtualSensorConfiguration( ).getMainClassInitialParams( ) ;
+		String param = null;
+		param = params.get(WINDOW_SIZE);
+		if (param != null) windowSize = Integer.parseInt(param) ;
+		else {
+			logger.error("The required parameter: >" + WINDOW_SIZE + "<+ is missing.from the virtual sensor configuration file.");
+			return false;
+		}
+		param = params.get(STEP_SIZE);
+		if (param != null) stepSize = Integer.parseInt(param) ;
+		else {
+			logger.error("The required parameter: >" + STEP_SIZE + "<+ is missing.from the virtual sensor configuration file.");
+			return false;
+		}
+		if (windowSize < stepSize) {
+			logger.error("The parameter " + WINDOW_SIZE + " must be greater or equal to the parameter " + STEP_SIZE);
+			return false;
+		}
+		return init();
+	}
+
+	public void dataAvailable(String inputStreamName, StreamElement streamElement) {
+		ArrayBlockingQueue<StreamElement> circularBuffer = circularBuffers.get(inputStreamName) ;
+		// Get the circular buffer that matches the input stream. Create a new one if none exists
+		if (circularBuffer == null) {
+			circularBuffer = new ArrayBlockingQueue<StreamElement> (windowSize);
+			circularBuffers.put(inputStreamName, circularBuffer) ;
+		}
+		// Put the new stream element in the circular buffer and call the process method if the number of elements
+		// in the buffer is more or equal to the step size
+		try {
+			circularBuffer.put(streamElement);
+			
+			logger.debug("Circular buffer for stream " + inputStreamName + " contains: " + circularBuffer.size() + " elements (" + circularBuffer.remainingCapacity() + " free places remains)");
+			
+			if (circularBuffer.size() >= stepSize) {
+
+				double[][] values = new double[stepSize][streamElement.getData().length] ;
+				long[] timestamps = new long[stepSize] ;
+				
+				StreamElement elt = null;
+				double[] dataInDouble = null;
+				for (int i = 0 ; i < stepSize ; i++) {
+					try {
+						elt = circularBuffer.take();
+						dataInDouble = new double[elt.getData().length] ;
+						for (int j = 0 ; j < dataInDouble.length ; j++) {
+							try {
+								dataInDouble[j] = ((Number)elt.getData()[j]).doubleValue() ;
+							}
+							catch (java.lang.ClassCastException e) {
+								dataInDouble[j] = Double.NaN;
+								logger.warn("The element +>" + elt.getData()[j] + "<+ is not a number. This element has been converted to NaN.");
+							}
+						}
+						values[i] = dataInDouble;
+						timestamps[i] = new Long(elt.getTimeStamp());
+					} catch (InterruptedException e) {
+						logger.warn(e.getMessage(), e) ;
+					}
+				}
+				processNextWindow(values, timestamps);
+			}		
+		} catch (InterruptedException e) {
+			logger.warn (e.getMessage(), e) ;
+		}
+	}
+
+	public void finalize() {
+
+	}
+
+	public int getPredicateValueAsIntWithException (String parameter) {
+		String param = getVirtualSensorConfiguration( ).getMainClassInitialParams( ).get(parameter);
+		if (param == null) throw new  java.lang.RuntimeException ("The required parameter: >" + parameter + "<+ is missing.from the virtual sensor configuration file.");
+		else return Integer.parseInt(param);
+	}
+	
+	public void processNextWindow (double [][] values , long[] timestamps) {
+		
+		FFTOutputPacket fftOutputPacket = process(values, timestamps);
+		
+		Serializable[] data = new Serializable[3];
+		data[0] = fftOutputPacket.getTimestamp();
+		data[1] = fftOutputPacket.getDf();
+		
+		StringBuilder sb = new StringBuilder();
+		int size = fftOutputPacket.getValues().size();
+		if (size != 0) sb.append(fftOutputPacket.getValues().get(0));
+		for (int i = 1 ; i < size ; i++) {
+			sb.append("," + fftOutputPacket.getValues().get(i));
+		}
+		data[2] = sb.toString().getBytes();
+		StreamElement se = new StreamElement (outputStructure, data) ;
+		dataProduced( se );
+	}
+
+	public abstract FFTOutputPacket process(double [][] values , long[] timestamps);
+
+	public abstract boolean init();
 }
