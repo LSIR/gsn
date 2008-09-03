fb451ef4312e4c4af32e0335765bda03f0fd03b2
tmaret <timothee.maret@gmail.com>
2008-09-03 08:38:36 +0000

    RoR web interface,
    *) Moved from Prototype to jQuery (removed the Protoype and scriptaculous javascript files)
    *) Implemented the search for Virtual Sensors
    *) Added the Flexigrid widget

--- web_app/app/controllers/gsn_controller.rb
+++ web_app/app/controllers/gsn_controller.rb
@@ -5,7 +5,7 @@ require 'jdbc'		# jruby -S gem install jdbc-wrapper
 
 class GsnController < ApplicationController
 	
-  protect_from_forgery :except => :notify 
+  protect_from_forgery :except => [:notify, :populate_search_list_of_vs]
 	
   def test
     p CONTAINER_CONFIG.jdbc_username
@@ -28,16 +28,64 @@ class GsnController < ApplicationController
   def fullmap
   end
 
+  # Return the GSN XML Output list of virtual sensors for backward compatibility
+  def list_of_vs
+    xml_output = CONTAINER_INFO_HANDLER.build_output(params[:name])
+    render :xml => xml_output
+  end
+
+  def jason_search_list_of_vs
+
+    page	=   params[:page]	?   params[:page].to_i	:   1
+    rp		=   params[:rp]		?   params[:rp].to_i	:   30
+    query	=   params[:query]	?   params[:query]	:   ''
+    qtype	=   params[:qtype]	?   params[:qtype]	:   'get_name'
+    sortorder	=   params[:sortorder]	?   params[:sortorder]	:   'desc'
+    sortname	=   'get_name'
+
+    offset = ((page-1) * rp).to_i
+
+    @vss = filter_list_of_vs(query.split(' '), qtype.to_a, sortname, sortorder)
+    @vss_limited = @vss.length > rp ? @vss[offset, rp] : @vss
+
+    list_of_vs = Hash.new()
+    list_of_vs[:page] = page
+    list_of_vs[:total] = @vss.length
+    list_of_vs[:rows] = @vss_limited.collect{ |vs| {:id => vs.get_name.to_s, :cell => [
+	  vs.get_name,
+	  "TODO Last Update",
+	  vs.get_description,
+	  vs.is_enabled,]}}
+    render :text => list_of_vs.to_json, :layout => false
+  end
+
   def populate_search_list_of_vs
-    search_criteria = params[:search_criteria] ? params[:search_criteria].to_s : ''
-    current_page = params[:page] ? params[:page].to_i : 1
-    @per_page = 2 #TODO ADD THIS CONSTANT IN THE PREFERENCES
-    @vss = filter_list_of_vs(search_criteria)
-    @page_results = WillPaginate::Collection.create(current_page, @per_page, @vss.length) do |pager|
-	start = (current_page-1)*@per_page
-	pager.replace(@vss[start, @per_page])
+
+    page	=   params[:page]	?   params[:page].to_i	:   1
+    rp		=   params[:rp]		?   params[:rp].to_i	:   30
+    query	=   params[:query]	?   params[:query]	:   ''
+    qtype	=   params[:qtype]	?   params[:qtype].to_a	:   ['get_name','get_description']
+    sortorder	=   params[:sortorder]	?   params[:sortorder]	:   'desc'
+    sortname	=   'get_name'
+    offset = ((page-1) * rp).to_i
+
+    @vss = filter_list_of_vs(query.split(' '), qtype, sortname, sortorder)
+
+    if false #TODO Add a choice in the preferences
+      # Drop down search
+      @page_results = WillPaginate::Collection.create(page, rp, @vss.length) do |pager|
+	pager.replace(@vss[offset, rp])
+      end
+      render :partial => 'shared/search_list_of_vs', :locals => {:page_results => @page_results} #, :locals => {:loaded_vs_list => VS_ENABLED, :storage_manager => STORAGE_MANAGER}
+    else
+      # Flexigrid search      
+      @vss_limited = @vss.length > rp ? @vss[offset, rp] : @vss
+      list_of_vs = Hash.new()
+      list_of_vs[:page] = page
+      list_of_vs[:total] = @vss.length
+      list_of_vs[:rows] = @vss_limited.collect{ |vs| {:id => vs.get_name.to_s, :cell => [vs.get_name, "TODO Last Update", vs.get_description, vs.is_enabled,]}}
+      render :text => list_of_vs.to_json, :layout => false
     end
-    render :partial => 'shared/search_list_of_vs', :locals => {:page_results => @page_results} #, :locals => {:loaded_vs_list => VS_ENABLED, :storage_manager => STORAGE_MANAGER}
   end
 
   def update_refreshbar
@@ -69,16 +117,50 @@ class GsnController < ApplicationController
   end
   
   def notify
-  	puts 'called'
-  	puts params[:code]
-  	params.each {|key,value| puts "#{key} ==== #{value}" }
-  	render :text => "Accepted.", :layout => false,:status => 201
+    puts 'called'
+    puts params[:code]
+    params.each {|key,value| puts "#{key} ==== #{value}" }
+    render :text => "Accepted.", :layout => false,:status => 201
   end
 
   :private
 
-  def filter_list_of_vs(search_criteria)
-    VS_ENABLED.values.to_array.find_all { |vs| vs.get_name.include? search_criteria  }
+  # Returns a sorted and filtered list of VS (enabled or not).
+  # The VS are selected only if at least one of the VS field
+  # specified in the 'list_of_fields' contains one of the string
+  # specified in the 'list_of_criteria'. Comparison is not case
+  # sensitive.
+  #
+  # The VS are sorted by the 'sort_by_field' field and following
+  # the 'sort_order' order (either 'asc' or 'desc')
+  #
+  def filter_list_of_vs(list_of_criteria, list_of_fields, sort_by_field, sort_order)
+    sorted_list_of_vs = get_list_of_vs.sort_by { |vs| vs.send(sort_by_field.to_s) }
+    sorted_list_of_vs.reverse! if sort_order.eql? 'desc'
+    if list_of_criteria.length == 0
+      sorted_list_of_vs
+    else
+      filtered_list = sorted_list_of_vs.find_all { |vs|
+	list_of_criteria.inject(false) { |decide,criterion|
+	  (list_of_fields.inject(false) { |_decide, field| (vs.send(field.to_s).downcase.include? criterion.downcase) || _decide }) || decide }
+      }
+    end
+  end
+
+  # Returns an array of VS that merges the disabled and enabled ones.
+  # Use the is_enabled method to differentiate between these two groups.
+  def get_list_of_vs
+    enabled = VS_ENABLED.values.to_array.collect { |vs| def vs.is_enabled
+	true
+      end
+      vs
+    }
+    disabled = VS_DISABLED.values.to_array.select { |vs| vs }.collect { |vs| def vs.is_enabled
+	false
+      end
+      vs
+    }
+    enabled + disabled
   end
 	
 end
--- web_app/public/javascripts/application.js
+++ web_app/public/javascripts/application.js
@@ -4,11 +4,45 @@
  */
 $j = jQuery.noConflict();
 
+(function($) {
+  $(function() {
+    /* Uses the jQuery selector */
+  });
+})(jQuery);
 
 /* Use this to add behavior to DOM elements once the page is loaded. */
 jQuery(function() {
-    /*
-     * $j("#container #main").hide();
-     * alert('Content hidden by jQuery');
-     */
+
+    /* Use the Flexigrid jQuery plugin to show the list of VS */
+
+    $j("#search_vs_bar").flexigrid
+	(
+	{
+			url: 'populate_search_list_of_vs',
+			dataType: 'json',
+			colModel : [
+				{display: 'Name', name: 'get_name', width: 160, sortable: false, align: 'left', hide: false },
+				{display: 'Last Update', name: 'get_last_update', width: 160, sortable: false, align: 'left', hide: false},
+				{display: 'Description', name: 'get_description', width: 324, sortable: false, align: 'left', hide: false},
+				{display: 'Enabled', name: 'is_enabled', width: 40, sortable: false, align: 'center', hide: false}
+				],
+			searchitems : [
+				{display: 'Name', name : 'get_name', isdefault: true},
+				{display: 'Description', name : 'get_description'}
+				],
+			/*sortname: "get_name",*/
+			sortorder: "asc",
+			usepager: true,
+			title: 'Virtual Sensors',
+			useRp: true,
+			rpOptions: [10,50,100,500],
+			hideOnSubmit: false,
+			rp: 10,
+			singleSelect: true,
+			resizable: false,
+			showTableToggleBtn: true,
+			width: 750,
+			height: 120
+	}
+	);
 })
