b75b1ff5dbfedb7b8cef21dd289bca89a8e01bd3
tmaret <timothee.maret@gmail.com>
2008-09-08 08:21:19 +0000

    RoR web interface, get back the Prototype javascript library

--- web_app/env_setup.rb
+++
@@ -1,7 +0,0 @@
-$LOAD_PATH << 'META-INF/jruby.home/lib/ruby/site_ruby/1.8'
-
-RAILS_ROOT = File.expand_path '.'
-ENV['GEM_PATH']="#{RAILS_ROOT}/../ruby_gems"
-ENV['GEM_HOME']=ENV['GEM_PATH']
-$LOAD_PATH << 'META-INF/jruby.home/lib/ruby/site_ruby/1.8'
-$LOAD_PATH << ENV['GEM_PATH']
