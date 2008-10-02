1270dd00db816b010b502ed2974747f22635bd13
tmaret <timothee.maret@gmail.com>
2008-10-02 14:21:46 +0000

    Sensorscope Record stations, added
    *) VSD for stations 1 and 2
    *) .h file to generate the Java messages classes with MIG

--- virtual-sensors/record/sensorscope.h
+++ virtual-sensors/record/sensorscope.h
@@ -1,5 +1,6 @@
 /****************************************************************************
-   Sensorscope, EPFL, Deployment Messages, marett
+   GSN - EPFL - LSIR
+   Record stations data messages (Sensorscope Project)
    Use the following command in order to generate Java classes with MIG:
    
 	mig -target=tinynode \
@@ -13,13 +14,18 @@
  ***************************************************************************/
 
 enum {
-  AM_THURBOARD1DATA = 180,
-  AM_THURBOARD2DATA = 180
+  AM_THURBOARD1DATA = 128,
+  AM_THURBOARD2DATA = 128
 };
 
 
 typedef nx_struct thurBoard1Data {
-	nx_uint16_t	wind_speed				;
+        nx_uint8_t      ntw_sender_id           :       8       ;
+        nx_uint8_t      ntw_cost_to_bs          :       8       ;
+        nx_uint8_t      tsp_hop_count           :       8       ;
+        nx_uint8_t      tsp_packet_sn           :       8       ;
+        nx_uint8_t      reporter_id             :       8       ;
+        nx_uint32_t     timestamp               :       32      ;
 	nx_uint16_t	wind_direction		:	12	;
 	nx_uint16_t	solar_radiation		:	12	;
 	nx_uint16_t	soil_moisture_1		:	12	;
@@ -33,8 +39,15 @@ typedef nx_struct thurBoard1Data {
 	nx_uint16_t	soil_conductivity_3	:	12	;
 } thurBoard1Data_t;
 
+
 typedef nx_struct thurBoard2Data {
-	nx_uint16_t	air_temperature				;
+	nx_uint8_t	ntw_sender_id		:	8	;
+	nx_uint8_t	ntw_cost_to_bs		:	8	;
+	nx_uint8_t	tsp_hop_count		:	8	;
+	nx_uint8_t	tsp_packet_sn		:	8	;
+	nx_uint8_t	reporter_id		:	8	;
+	nx_uint32_t	timestamp		:	32	;
+	nx_uint16_t	air_temperature		:	16	;
 	nx_uint16_t	air_humidity		:	12	;
 	nx_uint16_t	surface_temperature	:	12	;	/* Add 3730 to that value */
 	nx_uint16_t	soil_moisture_1		:	12	;
