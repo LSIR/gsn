dd7c88dba32a4900d04e21a62120eb6227f6bdc8
erirag <erirag@users.sourceforge.net>
2008-10-31 10:33:18 +0000

    Modifications made to make the web application work with the new Rails version 2.1.2

--- web_app/config/environment.rb
+++ web_app/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -68,37 +68,3 @@ end
 
 require 'custom_validations'
 
-
-require 'singleton'
-require 'java'
-
-class JavaBeans
-
-  include Singleton
-
-  attr_accessor :vs_enabled, :vs_name_to_config, :vs_disabled, :storage_manager, :container_info_handler
-
-  def initialize
-    puts "Loading the GSN Java objects..."
-
-    @container_config = Java::gsn.Main::loadContainerConfig("#{RAILS_ROOT}/../conf/gsn.xml")
-    @vs_list = Java::gsn.gui.util::VSensorIOUtil.new(Java::gsn.Main.DEFAULT_VIRTUAL_SENSOR_DIRECTORY, 'virtual-sensors/Disabled/')
-    @enabled_vs_files = @vs_list.read_virtual_sensors
-    @disabled_vs_files = @vs_list.read_disabled_virtual_sensors
-
-    @vs_enabled = Java::gsn.gui.util::VSensorConfigUtil.getVSensorConfigs(@enabled_vs_files)
-    @vs_name_to_config = {}
-    @vs_enabled.each {|k| @vs_name_to_config[k[1].name.chomp.upcase] = k[1] }
-    @vs_disabled =  Java::gsn.gui.util::VSensorConfigUtil.getVSensorConfigs(@disabled_vs_files)
-    @storage_manager = Java::gsn.storage::StorageManager.getInstance()
-    @storage_manager.init(@container_config.jdbc_driver, @container_config.jdbc_username, @container_config.jdbc_password, @container_config.jdbc_url)
-    @container_info_handler = Java::gsn.http::ContainerInfoHandler.new
-  end
-
-  def ruby_to_java_string_a(ruby_array)
-    java_array = Java::java.lang.String[ruby_array.size]::new
-    ruby_array.each_with_index { |a_string,i| java_array[i] = a_string }
-    return java_array
-  end
-
-end
