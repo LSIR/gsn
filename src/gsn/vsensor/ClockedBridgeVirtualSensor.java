662c1dd4545e2e937aaa878b2028b575753e085a
ali_salehi <salehiam@gmail.com>
2009-02-23 19:36:16 +0000

    updated the clocked virtual sensor:

--- src/gsn/vsensor/ClockedBridgeVirtualSensor.java
+++ src/gsn/vsensor/ClockedBridgeVirtualSensor.java
@@ -3,71 +3,88 @@ package gsn.vsensor;
 import gsn.beans.StreamElement;
 import org.apache.log4j.Logger;
 import gsn.storage.StorageManager;
+import gsn.storage.DataEnumerator;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Timer;
 import java.util.TreeMap;
+import java.sql.SQLException;
 
 public class ClockedBridgeVirtualSensor extends AbstractVirtualSensor implements ActionListener {
-	
-	private static final String RATE_PARAM       = "rate";
-	private static final String TABLE_NAME_PARAM = "table_name";
-	
-	private Timer timer;
-	private int clock_rate;
-	private String table_name;
-	private long last_updated;
 
-	private static final transient Logger logger = Logger.getLogger( ClockedBridgeVirtualSensor.class );
+    private static final String RATE_PARAM       = "rate";
+    private static final String TABLE_NAME_PARAM = "table_name";
 
-	public boolean initialize ( ) {
+    private Timer timer;
+    private int clock_rate;
+    private String table_name;
+    private long last_updated;
 
-		TreeMap<String, String> params = getVirtualSensorConfiguration().getMainClassInitialParams();
+    private static final transient Logger logger = Logger.getLogger( ClockedBridgeVirtualSensor.class );
 
-		String rate_value = params.get(RATE_PARAM);
-		
-		if (rate_value == null){
-			logger.warn("Parameter \"rate\" not provider in Virtual Sensor file");
-			return false;
-		}
-		
-		clock_rate = Integer.parseInt(rate_value);
-		
-		String table_name_value = params.get(TABLE_NAME_PARAM);
-		
-		if (table_name_value == null){
-			logger.warn("Parameter \"table_name\" not provider in Virtual Sensor file");
-			return false;
-		}
-		
-		table_name = table_name_value;
-		
-		timer = new Timer( clock_rate , this );
-		
-		timer.start( );
-		
-		// TODO read current time
-		last_updated = -1 ;
-		
-		return true;
-	}
+    public boolean initialize ( ) {
 
-	public void dataAvailable ( String inputStreamName , StreamElement data ) {
-		dataProduced( data );
-		if ( logger.isDebugEnabled( ) ) logger.debug( "Data received under the name: " + inputStreamName );
-	}
+        TreeMap<String, String> params = getVirtualSensorConfiguration().getMainClassInitialParams();
 
-	public void finalize ( ) {
-		timer.stop( );
+        String rate_value = params.get(RATE_PARAM);
 
-	}
-	
-	  public void actionPerformed ( ActionEvent actionEvent ) {
-		  
-		  /*check if new data is posted*/
-		  /*call dataProduced(StreamElement se)*/
-		  StorageManager.
+        if (rate_value == null){
+            logger.warn("Parameter \"rate\" not provider in Virtual Sensor file");
+            return false;
+        }
+
+        clock_rate = Integer.parseInt(rate_value);
+
+        String table_name_value = params.get(TABLE_NAME_PARAM);
+
+        if (table_name_value == null){
+            logger.warn("Parameter \"table_name\" not provider in Virtual Sensor file");
+            return false;
+        }
+
+        table_name = table_name_value;
+
+        timer = new Timer( clock_rate , this );
+
+        timer.start( );
+
+        // TODO read current time
+        last_updated = -1 ;
+
+        return true;
+    }
+
+    public void dataAvailable ( String inputStreamName , StreamElement data ) {
+        dataProduced( data );
+        if ( logger.isDebugEnabled( ) ) logger.debug( "Data received under the name: " + inputStreamName );
+    }
+
+    public void finalize ( ) {
+        timer.stop( );
+
+    }
+
+    public void actionPerformed ( ActionEvent actionEvent ) {
+
+        /*check if new data is posted*/
+        /*call dataProduced(StreamElement se)*/
+        StringBuilder query = new StringBuilder("select * from "+table_name+" where timed > "+last_updated+" order by timed asc");
+
+        try {
+            DataEnumerator data = StorageManager.getInstance().executeQuery(query,true);
+            while (data.hasMoreElements()){
+                StreamElement se = data.nextElement();
+                last_updated = se.getTimeStamp();
+                dataProduced(se);
+            }
+        } catch (SQLException e) {
+            logger.error(e.getMessage(),e);
+        }
+
+
+
+
 //		    StreamElement streamElement = new StreamElement( EMPTY_FIELD_LIST , EMPTY_FIELD_TYPES , EMPTY_DATA_PART , actionEvent.getWhen( ) );
 //		    if(delayPostingElements){
 //		    	streamElementBuffer.add(streamElement);
@@ -77,6 +94,6 @@ public class ClockedBridgeVirtualSensor extends AbstractVirtualSensor implements
 //		    }
 //		    else
 //		    	postStreamElement( streamElement );
-		  }
+    }
 
 }
