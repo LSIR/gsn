096ec67fb599c5be36849c15788954aabd4d7eaa
mriahi <mehdi.riahi@epfl.ch>
2009-05-06 14:17:04 +0000

    Added a method to dynamically build hibernate sessionFactory

--- core/src/java/gsn/beans/HStorageManager.java
+++
@@ -1,6 +0,0 @@
-package gsn.beans;
-
-
-public class HStorageManager {
-    //SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-}
---
+++ core/src/test/gsn/storage/TestHStorageManage.java
@@ -0,0 +1,76 @@
+package gsn.storage;
+
+import org.junit.Test;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.classic.Session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.Serializable;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TestHStorageManage {
+
+    @Test
+    public void testGetSessionFactoryH2() {
+        String dialect = "org.hibernate.dialect.H2Dialect";
+        String driverClass = "org.h2.Driver";
+        String url = "jdbc:h2:mem:gsn_mem_db";
+        String username = "";
+        String password = "";
+
+        String entityMapping =
+                "<!DOCTYPE hibernate-mapping PUBLIC\n" +
+                        "\"-//Hibernate/Hibernate Mapping DTD 3.0//EN\"\n" +
+                        "\"http://hibernate.sourceforge.net/hibernate-mapping-3.0.dtd\">" +
+                        "<hibernate-mapping>\n" +
+                        "\n" +
+                        "    <class entity-name=\"Customer\">\n" +
+                        "\n" +
+                        "        <id name=\"id\"\n" +
+                        "            type=\"long\"\n" +
+                        "            column=\"ID\">\n" +
+                        "            <generator class=\"native\"/>\n" +
+                        "        </id>\n" +
+                        "\n" +
+                        "        <property name=\"name\"\n" +
+                        "            column=\"NAME\"\n" +
+                        "            type=\"string\"/>\n" +
+                        "\n" +
+                        "        <property name=\"address\"\n" +
+                        "            column=\"ADDRESS\"\n" +
+                        "            type=\"string\"/>\n" +
+                        "\n" +
+                        "    </class>\n" +
+                        "    \n" +
+                        "</hibernate-mapping>";
+
+        System.out.println(entityMapping);
+        SessionFactory sessionFactory = HStorageManager.getSessionFactory(dialect, driverClass, url, username, password, entityMapping);
+        Session session = sessionFactory.getCurrentSession();
+
+        Transaction tx = session.beginTransaction();
+
+// Create a customer
+        Map david = new HashMap();
+        david.put("name", "David");
+
+// Save both
+        long id = (Long) session.save("Customer", david);
+        tx.commit();
+
+
+        assertEquals(id, 1L);
+
+
+        session = sessionFactory.getCurrentSession();
+        tx = session.beginTransaction();
+        Map savedDavid = (Map) session.get("Customer", id);
+        tx.commit();      
+        assertEquals(savedDavid.get("name"), "David");
+
+        sessionFactory.close();
+    }
+}
