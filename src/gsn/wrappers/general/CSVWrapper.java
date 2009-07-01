ed09154a928d2876dfe8fdeead767fe12a758522
ali_salehi <salehiam@gmail.com>
2009-07-01 17:21:11 +0000

    Removing duplicate error logs.

--- src/gsn/wrappers/general/CSVWrapper.java
+++ src/gsn/wrappers/general/CSVWrapper.java
@@ -47,47 +47,53 @@ public class CSVWrapper extends AbstractWrapper {
 		String timezone = addressBean.getPredicateValueWithDefault("timezone", handler.LOCAL_TIMEZONE_ID);
 		String nullValues = addressBean.getPredicateValueWithDefault("bad-values", "");
 		samplingPeriodInMsc = addressBean.getPredicateValueAsInt("sampling", 10000);
-		
+
 		if (csvSeparator!= null && csvSeparator.length()!=1) {
 			logger.warn("The provided CSV separator:>"+csvSeparator+"< should only have  1 character, thus ignored and instead \",\" is used.");
 			csvSeparator=",";
 		}
-		
+
 		if (csvStringQuote.length()!=1) {
 			logger.warn("The provided CSV quote:>"+csvSeparator+"< should only have 1 character, thus ignored and instead '\"' is used.");
 			csvStringQuote="\"";
 		}
 		try {
 			String checkPointFile = new File(checkPointDir).getAbsolutePath()+"/"+(new File(dataFile).getName())+"-"+addressBean.hashCode();
-	
+
 			if (handler.initialize(dataFile.trim(), csvFields, csvFormats, csvSeparator.toCharArray()[0], csvStringQuote.toCharArray()[0], skipFirstXLine, nullValues,timezone,checkPointFile)==false) 
 				return false;
 		}catch (Exception e) {
 			logger.error("Loading the csv-wrapper failed:" +e.getMessage(),e);  
 			return false;
 		}
-		
+
 		dataField = handler.getDataFields();
 		return true;
 	}
 
 	public void run ( ) {
 		Exception preivousError = null;
+		long previousModTime = -1;
 		while ( isActive( ) ) {
+			long lastModified = new File(handler.getDataFile()).lastModified();
 			try {
-				ArrayList<TreeMap<String, Serializable>> output = handler.run(new FileReader(handler.getDataFile()), checkPointDir);
-				for (TreeMap<String, Serializable> se : output) {
-					StreamElement streamElement = new StreamElement(se,getOutputFormat());
-					boolean insertionSuccess = postStreamElement(streamElement);
-					handler.updateCheckPointFile(streamElement.getTimeStamp());
+				ArrayList<TreeMap<String, Serializable>> output = null;
+				if (preivousError==null || (preivousError!=null && lastModified != previousModTime)){
+					output = handler.run(new FileReader(handler.getDataFile()), checkPointDir);
+					for (TreeMap<String, Serializable> se : output) {
+						StreamElement streamElement = new StreamElement(se,getOutputFormat());
+						boolean insertionSuccess = postStreamElement(streamElement);
+						handler.updateCheckPointFile(streamElement.getTimeStamp());
+					}
 				}
-				if (output.size()==0) //More intelligent sleeping, being more proactive once the wrapper receives huge files.
+				if (output==null || output.size()==0) //More intelligent sleeping, being more proactive once the wrapper receives huge files.
 					Thread.sleep(samplingPeriodInMsc);
 			}catch (Exception e) {
 				if (preivousError!=null && preivousError.getMessage().equals(e.getMessage()))
 					continue;
 				logger.error(e.getMessage()+" :: "+dataFile,e);
 				preivousError = e;
+				previousModTime=lastModified;
 			}
 		}
 	}
