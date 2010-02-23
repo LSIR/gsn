02105135853e9003c58be6e86ca8367e4b9e687b
yuecelm <yuecelm@users.sourceforge.net>
2010-02-23 19:36:37 +0000

    make calibration value optional for thermistor conversion functions

--- gsn/src/gsn/vsensor/permasense/Thermistor44005.java
+++ gsn/src/gsn/vsensor/permasense/Thermistor44005.java
@@ -14,14 +14,17 @@ public class Thermistor44005 implements Converter {
 	
 	public String convert(Serializable input, String value) {
 		String result = null;
+		double cal = 0.0;
 		//long start = System.nanoTime();
 		int v = ((Integer) input).intValue();
 		if (v < 64000 && v != 0) {
+			if (value != null)
+				cal = Double.parseDouble(value);
 			double ln_res = Math.log(10000.0 / ((64000.0 / v) - 1.0));
 			//Math.pow(v, 3.0) needs more CPU instructions than (v * v * v)
 			//double steinhart_eq = 0.0014051 + 0.0002369 * ln_res + 0.0000001019 * Math.pow(ln_res, 3);
 			double tmp = 0.0014051 + (0.0002369 * ln_res) + (0.0000001019 * (ln_res * ln_res * ln_res));
-			result = decimal4.format((1.0 / tmp) - 273.15);
+			result = decimal4.format((1.0 / tmp) - 273.15 - cal);
 		}
 		//if (logger.isDebugEnabled())
 		//	logger.debug("thermistor44005Conversion: " + Long.toString((System.nanoTime() - start) / 1000) + " us");
--- gsn/src/gsn/vsensor/permasense/Thermistor44006.java
+++ gsn/src/gsn/vsensor/permasense/Thermistor44006.java
@@ -14,14 +14,17 @@ public class Thermistor44006 implements Converter {
 	
 	public String convert(Serializable input, String value) {
 		String result = null;
+		double cal = 0.0;
 		//long start = System.nanoTime();
 		int v = ((Integer) input).intValue();
 		if (v < 64000 && v != 0) {
+			if (value != null)
+				cal = Double.parseDouble(value);
 			double ln_res = Math.log(27000.0 / ((64000.0 / v) - 1.0));
 			//Math.pow(v, 3.0) needs more CPU instructions than (v * v * v)
 			//double steinhart_eq = 0.00103348 + 0.000238465 * ln_res + 0.000000158948 * Math.pow(ln_res, 3);
 			double steinhart_eq = 0.00103348 + (0.000238465 * ln_res) + (0.000000158948 * (ln_res * ln_res * ln_res));
-			result = decimal4.format((1.0 / steinhart_eq) - 273.15 - Double.parseDouble(value));
+			result = decimal4.format((1.0 / steinhart_eq) - 273.15 - cal);
 		}
 		//if (logger.isDebugEnabled())
 		//	logger.debug("thermistor44006Conversion: " + Long.toString((System.nanoTime() - start) / 1000) + " us");				
