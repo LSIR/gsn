54239a17c6c2ddd1f40aefce8321aecb471ecb6c
tgsell <tgsell@ethz.ch>
2010-03-29 14:59:10 +0000

    script execution and service window timing implemented

--- gsn/tools/backlog/python/SchedulePlugin.py
+++ gsn/tools/backlog/python/SchedulePlugin.py
@@ -9,10 +9,13 @@ import struct
 import re
 import os
 import pickle
-from datetime import datetime
+import shlex
+import subprocess
+from datetime import datetime, timedelta
 from threading import Event
 
 import BackLogMessage
+#import tos
 from crontab import CronTab
 from AbstractPlugin import AbstractPluginClass
 
@@ -50,6 +53,9 @@ class SchedulePluginClass(AbstractPluginClass):
 
     # Command Structure
     TOS_CMD_STRUCTURE = [('command', 'int', 1), ('argument', 'int', 4)]
+    
+    # Backward looking time naming
+    BACKWARD_LOOK_NAME = 'backward_look'
     ############################################
     '''
     This plugin offers the functionality to 
@@ -62,12 +68,33 @@ class SchedulePluginClass(AbstractPluginClass):
     def __init__(self, parent, options):
         AbstractPluginClass.__init__(self, parent, options, DEFAULT_BACKLOG)
         
-        self._work = Event()
+        self._connectionEvent = Event()
+        self._scheduleEvent = Event()
+        self._stopEvent = Event()
         
         self._gsnconnected = False
         self._schedulereceived = False
         self._schedule = None
         self._stopped = False
+        
+        address = self.getOptionValue('tos_source_addr')
+        
+        if address is None:
+            raise TypeError('tos_source_addr not specified in config file')
+
+        # Initialize the serial source to the TinyNode
+        
+        # split the address (it should have the form serial@port:baudrate)
+#        source = address.split('@')
+#        if source[0] == 'serial':
+#            try:
+#                # try to open a connection to the specified serial port
+#                serial = tos.getSource(address, debug)
+#                self._serialsource = tos.AM(serial)
+#            except Exception, e:
+#                raise TypeError('could not initialize serial source: ' + e.__str__())
+#        else:
+#            raise TypeError('address type must be serial')
         
         if os.path.isfile(self.getOptionValue('schedule_file')+'.parsed'):
             try:
@@ -79,6 +106,8 @@ class SchedulePluginClass(AbstractPluginClass):
                 self.error(e.__str__())
         else:
             self.info('there is no local schedule file available')
+            
+        self._max_next_schedule_wait_delta = timedelta(minutes=int(self.getOptionValue('max_next_schedule_wait_minutes')))
     
     
     def getMsgType(self):
@@ -88,7 +117,7 @@ class SchedulePluginClass(AbstractPluginClass):
     def connectionToGSNestablished(self):
         self.debug('connection established')
         self._gsnconnected = True
-        self._work.set()
+        self._connectionEvent.set()
        
         
     def run(self):
@@ -99,8 +128,8 @@ class SchedulePluginClass(AbstractPluginClass):
             self._gsnconnected = True
         else:
             self.info('waiting for gsn to connect')
-            self._work.wait(int(self.getOptionValue('gsn_connect_timeout')))
-            self._work.clear()
+            self._connectionEvent.wait((int(self.getOptionValue('max_gsn_connect_wait_minutes')) * 60))
+            self._connectionEvent.clear()
             if self._stopped:
                 self.info('died')
                 return
@@ -108,18 +137,18 @@ class SchedulePluginClass(AbstractPluginClass):
         # if GSN is connected try to get a new schedule for a while
         if self._gsnconnected:
             timeout = 0
-            while timeout < int(self.getOptionValue('gsn_schedule_timeout')):
+            while timeout < (int(self.getOptionValue('max_gsn_get_schedule_wait_minutes')) * 60):
                 self.info('request schedule from gsn')
                 self.processMsg(self.getTimeStamp(), struct.pack('<B', self.GSN_TYPE_GET_SCHEDULE))
-                self._work.wait(3)
+                self._scheduleEvent.wait(3)
                 if self._stopped:
                     self.info('died')
                     return
-                if self._work.isSet():
+                if self._scheduleEvent.isSet():
                     break
                 timeout += 3
             
-            if timeout >= int(self.getOptionValue('gsn_schedule_timeout')):
+            if timeout >= int(self.getOptionValue('max_gsn_get_schedule_wait_minutes')):
                 self.warning('gsn has not answered on any schedule request')
         else:
             self.warning('gsn has not connected')
@@ -131,19 +160,64 @@ class SchedulePluginClass(AbstractPluginClass):
             self.info('died')
             return
         
-        
-        t = time.time()
-        nextschedules = self._schedule.getNextSchedules(datetime.utcnow())
-        self.debug('next schedules: %f s' % (time.time() - t))
-        for schedule in nextschedules:
-            self.debug('\t(' + str(schedule[0]) + ',' + str(schedule[1]) + ',' + str(schedule[2]) + ')')
+        dtnow = None
+        firstloop = True
+        stop = False
+        while not stop:
+            if dtnow and dtnow.second == datetime.utcnow().second:
+                dtnow = datetime.utcnow()
+                dtnow += timedelta(seconds=1)
+            else:
+                dtnow = datetime.utcnow()
+            
+            # get the next schedule(s) in time
+            t = time.time()
+            nextschedules = self._schedule.getNextSchedules(dtnow, firstloop)
+            self.debug('next schedule: %f s' % (time.time() - t))
+            firstloop = False
+            
+            for schedule in nextschedules:
+                self.debug('(' + str(schedule[0]) + ',' + str(schedule[1]) + ')')
+                dtnow = datetime.utcnow()
+                timediff = schedule[0] - dtnow
+                args = shlex.split(schedule[1])
+                if schedule[0] <= dtnow:
+                    self.debug('start >' + schedule[1] + '< now')
+                elif timediff < self._max_next_schedule_wait_delta:
+                    self.debug('start >' + schedule[1] + '< in ' + str(timediff.seconds + timediff.days * 86400) + ' seconds')
+                    self._stopEvent.wait(timediff.seconds + timediff.days * 86400)
+                    if self._stopped:
+                        break
+                else:
+                    self.debug('nothing more to do in the next ' + self.getOptionValue('max_next_schedule_wait_minutes') + ' minutes (max_next_schedule_wait_minutes)')
+                    stop = True
+                    break
+                
+                try:
+                    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                except Exception, e:
+                    self.error('error in schedule script >' + schedule[1] + '<:' + str(e))
+                else:
+                    out = proc.communicate()
+                    if out[0]:
+                        self.info(out[0])
+                    if out[1]:
+                        self.error(out[1])
+                    ret = proc.poll()
+                    if ret != 0:
+                        self.error('return code from schedule script >' + schedule[1] + '< is ' + str(ret))
+                    
+        self._shutdown()
                 
         self.info('died')
     
     
     def stop(self):
         self._stopped = True
-        self._work.set()
+        self._connectionEvent.set()
+        self._scheduleEvent.set()
+        self._stopEvent.set()
+        self._serialsource.stop()
         self.info('stopped')
 
 
@@ -187,15 +261,58 @@ class SchedulePluginClass(AbstractPluginClass):
                         self.info('using locally stored schedule file')
     
             self._schedulereceived = True
-            self._work.set()
+            self._scheduleEvent.set()
         else:
             self.info('schedule already received')
+
+
+    def _getCmdResponse(self, cmd, argument=0):
+        '''
+        Send a command to the TinyNode and return the response's argument
+        
+        @param cmd: the 1 Byte Command Code
+        @param argument: the 4 byte argument for the command
+
+        @return the response's argument on success and -1 on fail
+        '''
+        packet = tosPlugPacket.tos.Packet(self.TOS_CMD_STRUCTURE, [cmd, argument])
+
+        resp_packet = None
+        while not resp_packet:
+            self._serialsource.write(packet, self.AM_CONTROL_CMD_MSG)
+            resp_packet = self._serialsource.read(1)
+
+        response = tos.Packet(self.TOS_CMD_STRUCTURE, resp_packet['data'])
+        if response['command'] == cmd:
+            return response['argument']
+        else:
+            return -1
         
 
 
     def _shutdown(self):
-        # TODO: next wakeup and shutdown
-        pass
+        now = datetime.utcnow()
+        sc = ScheduleCron(fake_tab=(self.getOptionValue('service_wakeup_schedule') + ' ' + self.BACKWARD_LOOK_NAME + '=' + self.getOptionValue('service_wakeup_minutes')))
+        next_service_wakeup = sc.getNextSchedules(now, True)[0][0]
+        if next_service_wakeup < now + self._max_next_schedule_wait_delta:
+            td = (next_service_wakeup + timedelta(minutes=int(self.getOptionValue('service_wakeup_minutes')))) - now
+            waitfor = td.seconds + td.days * 86400 + 1
+            self.info('waiting ' + str(waitfor/60.0) + ' minutes for service windows to finish')
+            self._stopEvent.wait(waitfor)
+            now = datetime.utcnow()
+            next_service_wakeup = sc.getNextSchedules(now)[0]
+            #TODO: next schedule could be soon...
+        
+        # Syncronize Service Wakeup Time
+        time_delta = next_service_wakeup - now
+        time_to_service = time_delta.seconds + time_delta.days * 86400
+        self.debug('next service window is in ' + str(time_to_service/60.0) + ' minutes')
+        if self._getCmdResponse(self.CMD_SERVICE_WINDOW, time_to_service) == time_to_service:
+            self.info('ReverseModePlugin: Successfully scheduled the next service window wakeup (that\'s in '+str(time_to_service)+' minutes)')
+
+        # Get Wakeup Type
+        self._wakeup_reason = self._getCmdResponse(self.CMD_WAKEUP_QUERY)
+        self.info("ReverseModePlugin: TinyNode woke me up for reason code "+str(self._wakeup_reason))
             
         
         
@@ -208,27 +325,37 @@ class ScheduleCron(CronTab):
             self._scheduleSanityCheck(schedule)
         
     
-    def getNextSchedules(self, dt):
-        schedules = []
-        count = 1
+    def getNextSchedules(self, date_time, look_backward=False):
+        future_schedules = []
+        backward_schedules = []
+        now = datetime.utcnow()
         for schedule in self.crons:
-            wd = dt.isoweekday()
-            if wd == 7:
-                wd = 0
+            commandstring = str(schedule.command)
+            index = commandstring.lower().find(SchedulePluginClass.BACKWARD_LOOK_NAME)
+            td = timedelta()
+            if index != -1:
+                if look_backward:
+                    backwardmin = int(commandstring[index:].replace(SchedulePluginClass.BACKWARD_LOOK_NAME+'=',''))
+                    td = timedelta(minutes=backwardmin)
+                    nextdt = self._getNextSchedule(date_time - td, schedule)
+                    if nextdt < now:
+                        backward_schedules.append((nextdt, commandstring))
+                    
+                commandstring = commandstring[:index]
+                
+            nextdt = self._getNextSchedule(date_time, schedule)
+            if not future_schedules or nextdt < future_schedules[0][0]:
+                future_schedules = []
+                future_schedules.append((nextdt, commandstring))
+            elif nextdt == future_schedules[0][0]:
+                future_schedules.append((nextdt, commandstring))
             
-            nxt = self._getNextSchedule(dt, schedule)
-            if not schedules or nxt < schedules[0][0]:
-                schedules = []
-                schedules.append((nxt, count, schedule.command))
-            elif nxt == schedules[0][0]:
-                schedules.append((nxt, count, schedule.command))
-            count += 1
-        return schedules
+        return backward_schedules + future_schedules
     
     
-    def _getNextSchedule(self, dt, schedule):
+    def _getNextSchedule(self, date_time, schedule):
         second = 0
-        year = dt.year
+        year = date_time.year
             
         firsttimenottoday = True
         stop = False
@@ -245,14 +372,14 @@ class ScheduleCron(CronTab):
                         if wd == 0:
                             wd = 7
                         
-                        if nextdatetime < datetime(dt.year, dt.month, dt.day) or wd != nextdatetime.isoweekday():
+                        if nextdatetime < datetime(date_time.year, date_time.month, date_time.day) or wd != nextdatetime.isoweekday():
                             continue
                         
-                        if nextdatetime < datetime(dt.year, dt.month, dt.day+1):
+                        if nextdatetime < datetime(date_time.year, date_time.month, date_time.day+1):
                             for hour in self._getRange(schedule.hour()):
                                 for minute in self._getRange(schedule.minute()):
                                     nextdatetime = datetime(year, month, day, hour, minute)
-                                    if nextdatetime < dt:
+                                    if nextdatetime < date_time:
                                         continue
                                     stop = True
                                     break
