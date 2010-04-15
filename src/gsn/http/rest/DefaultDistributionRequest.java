16be709f7ea2e8b109874641945642c396d05982
mriahi <mehdi.riahi@epfl.ch>
2010-04-15 18:01:56 +0000

    Fixed and issue in equals and hashCode methods.

--- src/gsn/http/rest/DefaultDistributionRequest.java
+++ src/gsn/http/rest/DefaultDistributionRequest.java
@@ -78,5 +78,29 @@ public class DefaultDistributionRequest implements DistributionRequest {
 	public DeliverySystem getDeliverySystem() {
 		return deliverySystem;
 	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DefaultDistributionRequest that = (DefaultDistributionRequest) o;
+
+        if (deliverySystem != null ? !deliverySystem.equals(that.deliverySystem) : that.deliverySystem != null)
+            return false;
+        if (query != null ? !query.equals(that.query) : that.query != null) return false;
+        if (vSensorConfig != null ? !vSensorConfig.equals(that.vSensorConfig) : that.vSensorConfig != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = query != null ? query.hashCode() : 0;
+        result = 31 * result + (deliverySystem != null ? deliverySystem.hashCode() : 0);
+        result = 31 * result + (vSensorConfig != null ? vSensorConfig.hashCode() : 0);
+        return result;
+    }
 }
 	
