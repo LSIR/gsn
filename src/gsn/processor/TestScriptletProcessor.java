67ca017e9d233ee7abf7464087ea14533dcb5f2d
tmaret <timothee.maret@gmail.com>
2010-06-21 11:04:15 +0000

    Added support for periodical execution of code to the ScriptletProcessor. See the periodic-scriptlet.xml description file for an example.

--- src/gsn/processor/TestScriptletProcessor.java
+++ src/gsn/processor/TestScriptletProcessor.java
@@ -51,71 +51,29 @@ public class TestScriptletProcessor {
     public void testCorrectProcessorParameters() {
         ScriptletProcessor processor = new ScriptletProcessor();
         TreeMap<String,String> parameters = new TreeMap<String,String>();
-
-        boolean status = processor.initialize(dataFields1, parameters, new HashMap<ScriptletProcessor.StreamElementComparator, Binding>());
+        boolean status = processor.initialize(dataFields1, parameters);
         assertFalse(status);
-
+        //
+        parameters = new TreeMap<String,String>();
+        parameters.put("anyparam","println 'Hello World!';");
+        status = processor.initialize(dataFields1, parameters);
+        assertFalse(status);
+        //
+        parameters = new TreeMap<String,String>();
         parameters.put("scriptlet","println 'Hello World!';");
-        status = processor.initialize(dataFields1, parameters, new HashMap<ScriptletProcessor.StreamElementComparator, Binding>());
+        status = processor.initialize(dataFields1, parameters);
         assertTrue(status);
-    }
-
-    @Test
-    public void testStreamElementComparator () {
-        ScriptletProcessor.StreamElementComparator c1 = new ScriptletProcessor.StreamElementComparator(new StreamElement(dataFields1, data1));
-        assertNotNull(c1);
-        ScriptletProcessor.StreamElementComparator c2 = new ScriptletProcessor.StreamElementComparator(new StreamElement(dataFields1, data1));
-        assertNotNull(c2);
-        assertTrue(c1.equals(c2));
-        assertTrue(c2.equals(c1));
-
-        c1.getStreamElement().setData(0,1000);
-        assertTrue(c1.equals(c2));
-        assertTrue(c2.equals(c1));
-
-        ScriptletProcessor.StreamElementComparator c3 = new ScriptletProcessor.StreamElementComparator(new StreamElement(dataFields2, data2));
-        assertNotNull(c3);
-        assertFalse(c1.equals(c3));
-        assertFalse(c3.equals(c1));
-
-    }
-
-    @Test
-    public void testGroovyContextCache () {
-        ScriptletProcessor processor = getProcessor(dataFields1, "println 'Hello World ' + '!'; return gsn;");
-        assertNotNull(processor);
-        StreamElement se = new StreamElement(dataFields1, data1);
-        StreamElement se2 = new StreamElement(dataFields1, data1);
-        StreamElement se3 = new StreamElement(dataFields2, data2);
-
-        assertEquals(new ScriptletProcessor.StreamElementComparator(se), new ScriptletProcessor.StreamElementComparator(se2));
-        assertNotSame(new ScriptletProcessor.StreamElementComparator(se), new ScriptletProcessor.StreamElementComparator(se3));
-
-        Binding c1 = processor.getContext(se);
-        Binding c2 = processor.getContext(se2);
-        Binding c3 = processor.getContext(se3);
-
-        assertNotNull(c1);
-        assertNotNull(c2);
-        assertNotNull(c3);
-
-        HashMap<ScriptletProcessor.StreamElementComparator, Binding> cache = new HashMap<ScriptletProcessor.StreamElementComparator, Binding>();
-        ScriptletProcessor.StreamElementComparator sec = new ScriptletProcessor.StreamElementComparator(se);
-        ScriptletProcessor.StreamElementComparator sec2 = new ScriptletProcessor.StreamElementComparator(se2);
-        ScriptletProcessor.StreamElementComparator sec3 = new ScriptletProcessor.StreamElementComparator(se3);
-
-        assertTrue(sec.equals(sec));
-        assertTrue(sec.equals(sec2));
-        assertTrue(sec2.equals(sec));
-        assertFalse(sec.equals(sec3));
-        assertFalse(sec3.equals(sec));
-
-        cache.put(sec, new Binding());
-        
-        assertTrue(cache.containsKey(sec));
-        assertTrue(cache.containsKey(sec2));
-        assertFalse(cache.containsKey(sec3));
-
+        //
+        parameters = new TreeMap<String,String>();
+        parameters.put("scriplet-periodic","println 'Hello World!';");
+        status = processor.initialize(dataFields1, parameters);
+        assertFalse(status);
+        //
+        parameters = new TreeMap<String,String>();
+        parameters.put("scriplet-periodic","println 'Hello World!';");
+        parameters.put("period","2000");
+        status = processor.initialize(dataFields1, parameters);
+        assertTrue(status);
     }
 
     @Test
@@ -123,9 +81,9 @@ public class TestScriptletProcessor {
 
         ScriptletProcessor processor = getProcessor(dataFields1, "msg = 'Hello ' + gsn; def msg1 = 'This is a script internal variable.'");
         StreamElement se = new StreamElement(dataFields1, data1);
-        Binding context = processor.getContext(se);
+        Binding context = processor.updateContext(se);
         context.setVariable("gsn", new String("Groovy GSN"));
-        processor.evaluate(processor.script, context);
+        processor.evaluate(processor.scriptlet, se, true);
         assertNotNull(context.getVariable("msg"));
         assertEquals(context.getVariable("msg"), "Hello Groovy GSN");
 
@@ -141,14 +99,14 @@ public class TestScriptletProcessor {
     public void testStatefullScriptlet() {
        ScriptletProcessor processor = getProcessor(dataFields1, "msg = (binding.getVariables().get('msg')==null) ? '' : msg; msg = 'Hello World ' + msg + ' ' + gsn + '!'; println msg; return gsn;");
         StreamElement se = new StreamElement(dataFields1, data1);
-        Binding context = processor.getContext(se);
+        Binding context = processor.updateContext(se);
         context.setVariable("gsn", new String("Groovy GSN"));
-        processor.evaluate(processor.script, context);
+        processor.evaluate(processor.scriptlet, se, true);
         assertNotNull(context.getVariable("msg"));
         assertEquals(context.getVariable("msg"), "Hello World  Groovy GSN!");
 
         context.setVariable("msg", new String("Stateful"));
-        processor.evaluate(processor.script, context);
+        processor.evaluate(processor.scriptlet, se, true);
         assertEquals(context.getVariable("msg"), "Hello World Stateful Groovy GSN!");
     }
 
@@ -156,8 +114,8 @@ public class TestScriptletProcessor {
     public void testBindingOut() {
         ScriptletProcessor processor = getProcessor(dataFields2, "return;");
         StreamElement se = new StreamElement(dataFields1, data1);
-        Binding context = processor.getContext(se);
-        processor.evaluate(processor.script, context);
+        Binding context = processor.updateContext(se);
+        processor.evaluate(processor.scriptlet, se, true);
 
         StreamElement seo = processor.formatOutputStreamElement(context);
         assertNotNull(seo.getData("temperature"));
@@ -171,15 +129,15 @@ public class TestScriptletProcessor {
     public void testTimedField() {
         ScriptletProcessor processor = getProcessor(dataFields1, "return;");
         StreamElement se = new StreamElement(dataFields1, data1);
-        Binding context = processor.getContext(se);
-        processor.evaluate(processor.script, context);
+        Binding context = processor.updateContext(se);
+        processor.evaluate(processor.scriptlet, se, true);
 
         StreamElement seo = processor.formatOutputStreamElement(context);
         assertNotSame(seo.getTimeStamp(), 123456L);
 
         se.setTimeStamp(123456L);
-        context = processor.getContext(se);
-        processor.evaluate(processor.script, context);
+        context = processor.updateContext(se);
+        processor.evaluate(processor.scriptlet, se, true);
         seo = processor.formatOutputStreamElement(context);
         assertEquals(123456L, seo.getTimeStamp());
     }
@@ -188,25 +146,25 @@ public class TestScriptletProcessor {
     public void testScriptletExecutionWithCompilationException() {
         ScriptletProcessor processor = getProcessor(dataFields1, "prinltn 'This Groovy code has a syntax error;'");
         StreamElement se = new StreamElement(dataFields1, data1);
-        Binding context = processor.getContext(se);
-        processor.evaluate(processor.script, context); 
+        Binding context = processor.updateContext(se);
+        processor.evaluate(processor.scriptlet, se, true);
     }
 
     @Test(expected = groovy.lang.MissingPropertyException.class)
     public void testScriptletExecutionWithUnsetVariableException() {
         ScriptletProcessor processor = getProcessor(dataFields1, "println 'This variable is not set ' + thevar");
         StreamElement se = new StreamElement(dataFields1, data1);
-        Binding context = processor.getContext(se);
+        Binding context = processor.updateContext(se);
         context.setVariable("gsn", new String("Groovy GSN"));
-        processor.evaluate(processor.script, context);
+        processor.evaluate(processor.scriptlet, se, true);
     }
 
     @Test(expected = Exception.class)
     public void testScriptletExecutionWithSyntaxError() {
         ScriptletProcessor processor = getProcessor(dataFields1, "this code is not groovy");
         StreamElement se = new StreamElement(dataFields1, data1);
-        Binding context = processor.getContext(se);
-        processor.evaluate(processor.script, context);
+        Binding context = processor.updateContext(se);
+        processor.evaluate(processor.scriptlet, se, true);
     }
 
     //
@@ -215,7 +173,7 @@ public class TestScriptletProcessor {
         ScriptletProcessor processor = new ScriptletProcessor();
         TreeMap<String,String> parameters = new TreeMap<String,String>();
         parameters.put("scriptlet", scriptlet);
-        processor.initialize(outputStructure, parameters, new HashMap<ScriptletProcessor.StreamElementComparator, Binding>());
+        processor.initialize(outputStructure, parameters);
         return processor;
     }
 
