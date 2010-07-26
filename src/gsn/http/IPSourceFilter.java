65bc92f0b1c23ff4f842ddf5f49abd94ccddefc6
pbostani <pbostani@users.sourceforge.net>
2010-07-26 09:51:27 +0000

    Generalized IPSource filter.

--- src/gsn/http/IPSourceFilter.java
+++ src/gsn/http/IPSourceFilter.java
@@ -8,28 +8,27 @@ import java.io.IOException;
 
 public class IPSourceFilter implements Filter {
 
-    public final static String ALLOWED_IP = "131.107.151.211";
-
-    private static transient Logger logger = Logger.getLogger( FilterConfig.class );
+    private static transient Logger logger = Logger.getLogger( IPSourceFilter.class );
 
     private FilterConfig filterConfig;
 
+    private String[] allowedSrcIps = null;
+
     public void init(FilterConfig filterConfig) throws ServletException {
         this.filterConfig = filterConfig;
+        allowedSrcIps = filterConfig.getInitParameter("allowedIps").split(";");
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         String ip = request.getRemoteAddr();
 
-        logger.warn("IP: " + ip);
-
         HttpServletResponse httpResp = null;
 
         if (response instanceof HttpServletResponse)
             httpResp = (HttpServletResponse) response;
 
-        if (ALLOWED_IP.equals(ip)) {
+        if (isAllowed(ip)) {
             chain.doFilter(request, response);
         } else {
             logger.warn("IP: " + ip + " not allowed.");
@@ -39,4 +38,12 @@ public class IPSourceFilter implements Filter {
     }
 
     public void destroy() {}
+
+    private boolean isAllowed (String ip) {
+        for (String aip : allowedSrcIps) {
+            if (aip.equals(ip))
+                return true;
+        }
+        return false;
+    }
 }
