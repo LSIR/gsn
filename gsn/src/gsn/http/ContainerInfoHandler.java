f8368ecf5ea6fbe7ccaaf8ae8a928ee79c0cf005
tgsell <tgsell@ethz.ch>
2010-07-27 08:42:04 +0000

    readded 'return only one stream element even if there a several with max timed' from rlim (see r2081)

--- gsn/src/gsn/http/ContainerInfoHandler.java
+++ gsn/src/gsn/http/ContainerInfoHandler.java
@@ -208,7 +208,7 @@ public class ContainerInfoHandler implements RequestHandler {
    * @return
    */
   public static ArrayList<StreamElement> getMostRecentValueFor(String virtual_sensor_name) {
-    StringBuilder query=  new StringBuilder("select * from " ).append(virtual_sensor_name).append( " where timed = (select max(timed) from " ).append(virtual_sensor_name).append(")");
+    StringBuilder query=  new StringBuilder("select * from " ).append(virtual_sensor_name).append( " where timed = (select max(timed) from " ).append(virtual_sensor_name).append(") order by PK desc limit 1");
     ArrayList<StreamElement> toReturn=new ArrayList<StreamElement>() ;
     try {
       DataEnumerator result = Main.getStorage(virtual_sensor_name).executeQuery( query , true );
