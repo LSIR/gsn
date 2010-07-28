ad01357939b489a004ebd7d2dbb964453818f6f5
tgsell <tgsell@ethz.ch>
2010-07-28 14:13:10 +0000

    reorganize virtual sensors folder structure

--- gsn/virtual-sensors/ethz/enable-vs-set.py
+++ gsn/virtual-sensors/ethz/enable-vs-set.py
@@ -6,19 +6,18 @@ import time
 
 abort = False
 
-if len(sys.argv) != 2 and len(sys.argv) != 3:
+if len(sys.argv) != 3:
   print 'usage: enable-vs-set.py <target> <db>'
   sys.exit(-1)
 
 filepath = sys.path[0] + '/conf/' + sys.argv[1] + '.list'
 
 h2 = False
-if len(sys.argv) == 3:
-    if sys.argv[1].lower() == "h2":
-        h2 = True
-    elif sys.argv[1].lower() != "mysql":
-        print 'usage: <db> can only be h2 or mysql'
-        sys.exit(-1)
+if sys.argv[2].lower() == "h2":
+    h2 = True
+elif sys.argv[2].lower() != "mysql":
+    print 'usage: <db> can only be h2 or mysql'
+    sys.exit(-1)
 
 # check if first arguments points to valid file
 if not os.path.exists(filepath):
@@ -50,7 +49,7 @@ for line in fd:
   if h2:
       source = 'ethz/h2/' + link 
   else:
-      source = 'ethz/' + link 
+      source = 'ethz/mysql/' + link 
   if os.path.exists(source):
     if not os.path.exists(link):
       print link + ' -> ' + source
