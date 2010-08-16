fdf0304578a029c2c3dedc898d2193de9bf738ed
rlim <rlim@users.sourceforge.net>
2010-08-16 16:16:45 +0000

    add uptime info to webpage (missing js file)

---
+++ gsn/webapp/js/uptime.js
@@ -0,0 +1,29 @@
+var uptime = { 
+
+  s : 0,
+
+  update : function(){
+    uptime.s = uptime.s+1000;
+    var out = "uptime: ";
+    if (uptime.s > 86400000)
+      out = out + Math.floor(uptime.s / 86400000)+"d ";
+    if (uptime.s > 3600000)
+      out = out + Math.floor((uptime.s % 86400000) / 3600000)+"h ";
+    if (uptime.s > 60000)
+      out = out + Math.floor(((uptime.s % 86400000) % 3600000 )/ 60000)+"m ";
+    out = out + Math.floor((((uptime.s % 86400000) % 3600000 ) % 60000)/1000)+"s";
+
+    $("#gsn-uptime").text(out);
+    setTimeout("uptime.update()",1000); 
+  },
+
+  setTime : function(time) {
+    uptime.s = Math.floor(time / 1000) * 1000;
+    setTimeout("uptime.update()",1000 - time % 1000); 
+  },
+
+};
+
+$(function() {
+  $("#footer").append("<div style=\"position:relative\"><div id=\"gsn-uptime\" style=\"position:absolute; bottom:-25px; right:0px; text-align:right\"></div></div>");
+});
