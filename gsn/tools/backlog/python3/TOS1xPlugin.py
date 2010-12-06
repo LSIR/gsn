354e6991ef948d7c0e625fd9250408de1468c661
tgsell <tgsell@ethz.ch>
2010-12-06 10:52:15 +0000

    read acknowledge timeout set to 1 second (no double write)

--- gsn/tools/backlog/python3/TOS1xPlugin.py
+++ gsn/tools/backlog/python3/TOS1xPlugin.py
@@ -26,13 +26,13 @@ class TOS1xPluginClass(TOSPluginClass, AbstractPluginClass):
 
 
     def _sendCloseQueueCommand(self):
-        if self.sendTOSmsg(array.array('B', [0x02, 0x00, 0x01, 0x00, 0x50, 0x7D, 0x00, 0x80]).tolist(), 0x00, 0.2, True, 10):
+        if self.sendTOSmsg(array.array('B', [0x02, 0x00, 0x01, 0x00, 0x50, 0x7D, 0x00, 0x80]).tolist(), 0x00, 1, True, 10):
             self.info('waiting 35 seconds for close queue command')
             time.sleep(35)
 
 
     def _sendOpenQueueCommand(self):
-        return self.sendTOSmsg(array.array('B', [0x02, 0x00, 0x01, 0x00, 0x50, 0x7D, 0x01, 0x80]).tolist(), 0x00, 0.2, True, 10)
+        return self.sendTOSmsg(array.array('B', [0x02, 0x00, 0x01, 0x00, 0x50, 0x7D, 0x01, 0x80]).tolist(), 0x00, 1, True, 10)
         
 
     def _backlog2tos(self, message):
