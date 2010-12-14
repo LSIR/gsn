b506f4b3a639765951b66c3af7c26cb1cfa5e94c
sofiane sarni <sofiane.sarni@epfl.ch>
2010-12-14 19:10:21 +0000

    added PostGIS as spatial engine (in addition to JTS).Spatial DB parameters must be set in file conf/spatial.properties.

--- src/gsn/http/GeoDataServlet.java
+++ src/gsn/http/GeoDataServlet.java
@@ -11,16 +11,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class GeoDataServlet extends HttpServlet {
 
     private static transient Logger logger = Logger.getLogger(GeoDataServlet.class);
-    private boolean usePostGIS = false; // by default use JTS
+    private boolean usePostGIS = true; // by default use JTS
     private User user = null;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        CheckGISToolkitToUse();
+
         try {
 
             if (Main.getContainerConfig().isAcEnabled()) {
@@ -34,14 +39,17 @@ public class GeoDataServlet extends HttpServlet {
             String env = HttpRequestUtils.getStringParameter("env", null, request); // e.g. "POLYGON ((0 0, 0 100, 100 100, 100 0, 0 0))";
             String query = HttpRequestUtils.getStringParameter("query", null, request);
 
-            response.getWriter().write(runJTS(env, query));
+            if (usePostGIS)
+                response.getWriter().write(runPostGIS(env, query));
+            else
+                response.getWriter().write(runJTS(env, query));
 
         } catch (ParseException e) {
-            logger.warn(e.getMessage(), e) ;
+            logger.warn(e.getMessage(), e);
         }
     }
 
-   /*
+    /*
    * Searches within the list of provided sensors
    * for the ones matching AC credentials for the current user
    * */
@@ -80,8 +88,57 @@ public class GeoDataServlet extends HttpServlet {
 
     }
 
+    public String runPostGIS(String env, String query) throws ParseException {
+
+        StringBuilder response = new StringBuilder();
+
+        GetSensorDataWithGeoPostGIS.buildGeoIndex();
+
+        response.append("List of all sensors: \n" + GetSensorDataWithGeoPostGIS.getListOfSensors() + "\n");
+        response.append("Envelope: " + env + "\n");
+
+        ArrayList<String> sensors = GetSensorDataWithGeoPostGIS.getListOfSensors(env);
+        String matchingSensors = getMatchingSensors(sensors);
+
+        response.append("List of all sensors within envelope: \n" + matchingSensors + "\n");
+
+        response.append("Query:" + query + "\n");
+        response.append("Query result: \n");
+        response.append(GetSensorDataWithGeoPostGIS.executeQuery(env, matchingSensors.toString(), query));
+
+        return response.toString();
+
+    }
+
     public void doPost(HttpServletRequest request, HttpServletResponse res) throws ServletException, IOException {
         doGet(request, res);
+    }
+
+    public void CheckGISToolkitToUse() {
+
+        usePostGIS = false; // use JTS by default
+
+        Properties p = new Properties();
+        try {
+            p.load(new FileInputStream(GetSensorDataWithGeoPostGIS.CONF_SPATIAL_PROPERTIES_FILE));
+        }
+        catch (IOException e) {
+            p = null;
+            logger.warn(e.getMessage(), e);
+        }
+
+        if (p != null) {
+            String typeOFGIS = p.getProperty("type");
+            if (typeOFGIS != null)
+                if (typeOFGIS.trim().toLowerCase().equals("postgis")) {
+                    usePostGIS = true;
+                }
+        }
+
+        if (usePostGIS)
+            logger.warn("Using PostGIS");
+        else
+            logger.warn("Using JTS");
     }
 
 
