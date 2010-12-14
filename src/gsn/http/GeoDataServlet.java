cbc1e558657ebcd6d4d069fe896a8f3880cc0b06
sofiane sarni <sofiane.sarni@epfl.ch>
2010-12-14 17:49:24 +0000

    refactoring before adding postgis query method

--- src/gsn/http/GeoDataServlet.java
+++ src/gsn/http/GeoDataServlet.java
@@ -14,52 +14,70 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static gsn.http.GetSensorDataWithGeo.*;
-
-
 public class GeoDataServlet extends HttpServlet {
 
     private static transient Logger logger = Logger.getLogger(GeoDataServlet.class);
+    private boolean usePostGIS = false; // by default use JTS
+    private User user = null;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            User user = null;
+
             if (Main.getContainerConfig().isAcEnabled()) {
                 HttpSession session = request.getSession();
                 user = (User) session.getAttribute("user");
-                response.setHeader("Cache-Control","no-store");
+                response.setHeader("Cache-Control", "no-store");
                 response.setDateHeader("Expires", 0);
-                response.setHeader("Pragma","no-cache");
+                response.setHeader("Pragma", "no-cache");
             }
-            GetSensorDataWithGeo g = GetSensorDataWithGeo.getInstance();
-            g.buildGeoIndex();
+
             String env = HttpRequestUtils.getStringParameter("env", null, request); // e.g. "POLYGON ((0 0, 0 100, 100 100, 100 0, 0 0))";
             String query = HttpRequestUtils.getStringParameter("query", null, request);
 
-            response.getWriter().write("List of all sensors: \n" + getListOfSensors() + "\n");
-            response.getWriter().write("Envelope: " + env + "\n");
+            response.getWriter().write(runJTS(env, query));
 
-            //
-            ArrayList<String> sensors = g.getListOfSensors(env);
-            StringBuilder matchingSensors = new StringBuilder();
-            for (String vsName : sensors) {
-                if ( ! Main.getContainerConfig().isAcEnabled() || (user != null && (user.hasReadAccessRight(vsName) || user.isAdmin()))) {
-                    matchingSensors.append(vsName);
-                    matchingSensors.append(GetSensorDataWithGeo.SEPARATOR);
-                }
-            }
-            matchingSensors.setLength(matchingSensors.length() - 1); // remove the last SEPARATOR
-            //
-            //response.getWriter().write("List of all sensors within envelope: \n" + g.getListOfSensorsAsString(env) + "\n");
-            response.getWriter().write("List of all sensors within envelope: \n" + matchingSensors + "\n");
-
-
-            response.getWriter().write("Query:" + query + "\n");
-            response.getWriter().write("Query result: \n");
-            response.getWriter().write(executeQuery(env, matchingSensors.toString(), query));
         } catch (ParseException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            logger.warn(e.getMessage(), e) ;
         }
+    }
+
+   /*
+   * Searches within the list of provided sensors
+   * for the ones matching AC credentials for the current user
+   * */
+    public String getMatchingSensors(ArrayList<String> sensors) {
+        StringBuilder matchingSensors = new StringBuilder();
+
+        for (String vsName : sensors) {
+            if (!Main.getContainerConfig().isAcEnabled() || (user != null && (user.hasReadAccessRight(vsName) || user.isAdmin()))) {
+                matchingSensors.append(vsName);
+                matchingSensors.append(GetSensorDataWithGeo.SEPARATOR);
+            }
+        }
+        matchingSensors.setLength(matchingSensors.length() - 1); // remove the last SEPARATOR
+        return matchingSensors.toString();
+    }
+
+    public String runJTS(String env, String query) throws ParseException {
+
+        StringBuilder response = new StringBuilder();
+
+        GetSensorDataWithGeo.buildGeoIndex();
+
+        response.append("List of all sensors: \n" + GetSensorDataWithGeo.getListOfSensors() + "\n");
+        response.append("Envelope: " + env + "\n");
+
+        ArrayList<String> sensors = GetSensorDataWithGeo.getListOfSensors(env);
+        String matchingSensors = getMatchingSensors(sensors);
+
+        response.append("List of all sensors within envelope: \n" + matchingSensors + "\n");
+
+        response.append("Query:" + query + "\n");
+        response.append("Query result: \n");
+        response.append(GetSensorDataWithGeo.executeQuery(env, matchingSensors.toString(), query));
+
+        return response.toString();
+
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse res) throws ServletException, IOException {
