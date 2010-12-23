81035a3586f63802a21f381a6ecefbfc953e11f2
tgsell <tgsell@ethz.ch>
2010-12-23 16:08:32 +0000

    first preparation for raw/nav GPS data separation in GPSPlugin

--- gsn/src/gsn/wrappers/backlog/plugins/GPSPlugin.java
+++ gsn/src/gsn/wrappers/backlog/plugins/GPSPlugin.java
@@ -2,15 +2,40 @@ package gsn.wrappers.backlog.plugins;
 
 import java.io.Serializable;
 import java.nio.*;
+import java.util.Hashtable;
 
 import org.apache.log4j.Logger;
 
 import gsn.beans.DataField;
+import gsn.wrappers.BackLogWrapper;
 import gsn.wrappers.backlog.BackLogMessage;
 
 public class GPSPlugin extends AbstractPlugin {
+	private static final String GPS_DATA_TYPE = "gps-data-type";
 
-	private DataField[] dataField = {	new DataField("TIMESTAMP", "BIGINT"),
+	private static final String RAW_NAMING = "raw";
+	private static final String NAV_NAMING = "nav";
+
+	private static final DataField[] navDataField = {
+		    new DataField("TIMESTAMP", "BIGINT"),
+			new DataField("GENERATION_TIME", "BIGINT"),
+			new DataField("DEVICE_ID", "INTEGER"),
+			new DataField("UTC_POS_TIME", "DOUBLE"),
+			new DataField("LATITUDE", "DOUBLE"),
+			new DataField("LAT_HEMISPHERE", "DOUBLE"),
+			new DataField("LONGITUDE", "DOUBLE"),
+			new DataField("LONG_HEMISPHERE", "DOUBLE"),
+			new DataField("QUALITY", "SMALLINT"),
+			new DataField("NR_SATELLITES", "SMALLINT"),
+			new DataField("HDOP", "SMALLINT"),
+			new DataField("GEOID_HEIGHT", "SMALLINT"),
+			new DataField("HEIGHT_DIF", "SMALLINT"),
+			new DataField("AGE_DGPS", "SMALLINT"),
+			new DataField("CHECKSUM", "SMALLINT")
+			};
+
+	private static final DataField[] rawDataField = {
+		    new DataField("TIMESTAMP", "BIGINT"),
 			new DataField("GENERATION_TIME", "BIGINT"),
 			new DataField("DEVICE_ID", "INTEGER"),
 			new DataField("GPS_TIME", "INTEGER"),
@@ -24,7 +49,34 @@ public class GPSPlugin extends AbstractPlugin {
 			new DataField("LLI", "SMALLINT")
 			};
 	
+	private static final Hashtable<String, NameDataFieldPair> gpsNamingTable = new Hashtable<String, NameDataFieldPair>();
+	static
+	{
+		gpsNamingTable.put(NAV_NAMING, new NameDataFieldPair(1, navDataField));
+		gpsNamingTable.put(RAW_NAMING, new NameDataFieldPair(2, rawDataField));
+	}
+	
 	private final transient Logger logger = Logger.getLogger( GPSPlugin.class );
+
+	private String gpsDataType;
+	
+	
+	@Override
+	public boolean initialize ( BackLogWrapper backlogwrapper, String coreStationName, String deploymentName) {
+		try {
+			gpsDataType = getActiveAddressBean().getPredicateValueWithException(GPS_DATA_TYPE).toLowerCase();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			return false;
+		}
+		if (gpsNamingTable.get(gpsDataType) == null) {
+			logger.error("wrong " + GPS_DATA_TYPE + " predicate key specified in virtual sensor XML file! (" + GPS_DATA_TYPE + "=" + gpsDataType + ")");
+			return false;
+		}
+		logger.info("using GPS data type: " + gpsDataType);
+		
+		return true;
+	}
 	
 	@Override
 	public byte getMessageType() {
@@ -33,7 +85,7 @@ public class GPSPlugin extends AbstractPlugin {
 
 	@Override
 	public DataField[] getOutputFormat() {
-		return dataField;
+		return gpsNamingTable.get(gpsDataType).dataField;
 	}
 
 	@Override
@@ -46,32 +98,56 @@ public class GPSPlugin extends AbstractPlugin {
 		if (logger.isDebugEnabled())
 			logger.debug("message received from CoreStation with DeviceId: " + deviceId);
 		
+		Serializable[] data = null;
+
 		// Parse the Message
 		ByteBuffer buffer = ByteBuffer.allocate(packet.length);
 		buffer.put(packet);
 		
 		buffer.position(0);
 		buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+		short msgType = buffer.getShort();
 		
-		
-		int gpsTime = buffer.getInt();
-		short gpsWeek = buffer.getShort();
-		double carrierPhase = buffer.getDouble();
-		double pseudorange = buffer.getDouble();
-		double doppler = (double) buffer.getFloat();
-		short spaceVehicle = (short) buffer.get();
-		short measurementQuality = buffer.getShort();
-		short signalStrength = buffer.getShort();
-		short lli = (short) buffer.get();
-		
-		Serializable[] data = {timestamp, timestamp, deviceId, gpsTime, gpsWeek, carrierPhase, pseudorange, doppler, spaceVehicle, measurementQuality, signalStrength, lli};
-		
-		if( dataProcessed(System.currentTimeMillis(), data) ) {
-			ackMessage(timestamp, super.priority);
-		} else {
-			logger.warn("The message with timestamp >" + timestamp + "< could not be stored in the database.");
+		if (msgType == gpsNamingTable.get(gpsDataType).typeNumber) {
+			if (gpsDataType.equals(NAV_NAMING)) {
+				
+			}
+			else if (gpsDataType.equals(RAW_NAMING)) {
+				int gpsTime = buffer.getInt();
+				short gpsWeek = buffer.getShort();
+				double carrierPhase = buffer.getDouble();
+				double pseudorange = buffer.getDouble();
+				double doppler = (double) buffer.getFloat();
+				short spaceVehicle = (short) buffer.get();
+				short measurementQuality = buffer.getShort();
+				short signalStrength = buffer.getShort();
+				short lli = (short) buffer.get();
+				
+				data = new Serializable[]{timestamp, timestamp, deviceId, gpsTime, gpsWeek, carrierPhase, pseudorange, doppler, spaceVehicle, measurementQuality, signalStrength, lli};
+			}
+			else {
+				logger.warn("Wrong GPS data type spedified.");
+				return true;
+			}
+			
+			if( dataProcessed(System.currentTimeMillis(), data) ) {
+				ackMessage(timestamp, super.priority);
+			} else {
+				logger.warn("The message with timestamp >" + timestamp + "< could not be stored in the database.");
+			}
 		}
 		
 		return true;
+	}
+}
+
+class NameDataFieldPair {
+	protected Integer typeNumber;
+	protected DataField[] dataField;
+	
+	NameDataFieldPair(Integer typeNumber, DataField[] dataField) {
+		this.typeNumber = typeNumber;
+		this.dataField = dataField;
 	}
 }
