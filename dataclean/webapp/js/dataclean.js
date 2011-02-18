1eab258bad7de9b7baf5931e0b9cd5cd7c368d85
sofiane sarni <sofiane.sarni@epfl.ch>
2011-02-18 17:00:38 +0000

    added necessary js scripts

---
+++ dataclean/webapp/js/dataclean.js
@@ -0,0 +1,75 @@
+/**
+ * simulation
+ */
+
+
+
+
+/*
+ var from = document.getElementById("from");
+
+
+
+ function makeRequest() {
+ var url = addURLParm(baseURL, "REQUEST",200);
+ url = addURLParm(baseURL, "from",from);
+ url = addURLParm(baseURL, "to",to);
+ var request = createXMLHTTP();
+ request.open("get",url,false);
+ request.send(null);
+ }
+
+ */
+
+var DataClean = {
+
+    debugmode : false,
+    theData : null,
+    plot : null,
+    previewPlot : null,
+    orderOfDirtyPoint : null, // array for order of dirty point
+    errorOfDirtyPoint : null, // array for order of dirty point
+
+    station_names : ["lafouly_st_1033","lafouly_st_1034",
+        "lafouly_st_1035","lafouly_st_1036",
+        "lafouly_st_1037","lafouly_st_1039",
+        "lafouly_st_1040","lafouly_st_1041",
+        "lafouly_st_1042","lafouly_st_1043",
+        "lafouly_st_1044"],
+
+    station_longs : [7.106459, 7.116046,
+        7.12273, 7.119712,
+        7.129109, 7.117759,
+        7.107333, 7.108824,
+        7.104324, 7.108735,
+        7.115372],
+
+    station_lats : [45.89898, 45.906143,
+        45.904561, 45.90511,
+        45.904381, 45.866703,
+        45.898031, 45.896434,
+        45.899146, 45.900581,
+        45.865347],
+
+    showText: function (txt) {
+        alert(txt);
+    },
+
+    addURLParam : function (url, param, value) {
+        url += (url.indexOf("?")) == -1 ? "?" : "&";
+        url += encodeURIComponent(param) + "=" + encodeURIComponent(value);
+        return url;
+    },
+
+
+    selectUnselect: function(stationName) {
+        var list_sensors = document.getElementById("vs");
+
+        for (var i = 0; i < list_sensors.length; i++) {
+            if (list_sensors.options[i].value == stationName) {
+                list_sensors.selectedIndex = i;
+            }
+        }
+    }
+
+};
