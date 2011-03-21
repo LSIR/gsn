2ac7e9300294d04336b59c81edea75b989c6e937
tgsell <tgsell@ethz.ch>
2011-03-21 14:54:58 +0000

    gps raw demux virtual sensor finalized

--- gsn/src/gsn/vsensor/GPSRawDemuxBridgeVirtualSensor.java
+++ gsn/src/gsn/vsensor/GPSRawDemuxBridgeVirtualSensor.java
@@ -1,6 +1,8 @@
 package gsn.vsensor;
 
 import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import gsn.beans.DataField;
 import gsn.beans.StreamElement;
@@ -11,13 +13,16 @@ public class GPSRawDemuxBridgeVirtualSensor extends BridgeVirtualSensorPermasens
 	
 	private static final transient Logger logger = Logger.getLogger(GPSRawDemuxBridgeVirtualSensor.class);
 
-	private static final DataField[] dataField = {	new DataField("TIMESTAMP", "BIGINT"),
+	private static final DataField[] dataField = {
+						new DataField("TIMESTAMP", "BIGINT"),
 						new DataField("GENERATION_TIME", "BIGINT"),
 						new DataField("DEVICE_ID", "INTEGER"),
 						new DataField("POSITION", "INTEGER"),
+						new DataField("GPS_RAW_DATA_VERSION", "SMALLINT"),
+					    new DataField("GPS_SAMPLE_COUNT", "INTEGER"),
+					    new DataField("GPS_SATS", "INTEGER"),
 						new DataField("GPS_TIME", "INTEGER"),
 						new DataField("GPS_WEEK", "SMALLINT"),
-						new DataField("SVS", "SMALLINT"),
 						new DataField("CARRIER_PHASE", "DOUBLE"),
 						new DataField("PSEUDO_RANGE", "DOUBLE"),
 						new DataField("DOPPLER", "INTEGER"),
@@ -38,13 +43,44 @@ public class GPSRawDemuxBridgeVirtualSensor extends BridgeVirtualSensorPermasens
 		serialized_data[2] = data.getData(dataField[2].getName());
 		// position
 		serialized_data[3] = data.getData(dataField[3].getName());
+		// gps data version
+		serialized_data[4] = data.getData(dataField[4].getName());
 
 		if (((Integer)data.getData("gps_raw_data_version")) == 1) {
-			byte[] binaryData = (byte[]) data.getData("gps_raw_data");
-			// TODO: parse GPS RAW binary data and put it into serialized_data[4] to serialized_data[13]
-		}
+			// gps sample count
+			serialized_data[5] = data.getData(dataField[5].getName());
+			// gps sats
+			Integer sats = (Integer) data.getData("GPS_SATS");
+			serialized_data[6] = sats;
+			
+			ByteBuffer bbuffer = ByteBuffer.wrap((byte[]) data.getData("gps_raw_data"));
+			bbuffer.order(ByteOrder.LITTLE_ENDIAN);
+			
+			try {
+				serialized_data[7] = bbuffer.getInt();
+				serialized_data[8] = bbuffer.getShort();
 
-		data = new StreamElement(dataField, serialized_data, data.getTimeStamp());
-		super.dataAvailable(inputStreamName, data);
+				int s = (int) bbuffer.get() & 0xFF;
+				if (sats.compareTo(s) == 0) {
+					for (int i=0; i<s; i++) {
+						serialized_data[9] = bbuffer.getDouble();
+						serialized_data[10] = bbuffer.getDouble();
+						serialized_data[11] = bbuffer.getInt();
+						serialized_data[12] = (short) bbuffer.get() & 0xFF;
+						serialized_data[13] = (short) bbuffer.get();
+						serialized_data[14] = (short) bbuffer.get();
+						serialized_data[15] = (short) bbuffer.get() & 0xFF;
+
+						data = new StreamElement(dataField, serialized_data, data.getTimeStamp());
+						super.dataAvailable(inputStreamName, data);
+					}
+				}
+				else
+					logger.error("number of statellites (GPS_SATS=" + sats + ") is not equal to the number of satellites in GPS_RAW_DATA (" + s + ")");
+			}
+			catch (Exception e) {
+				logger.error(e);
+			}
+		}
 	}
 }
