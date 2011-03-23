f07081d38a2c76243443c4a0cd7393ed0baa23c2
tgsell <tgsell@ethz.ch>
2011-03-23 11:18:16 +0000

    added permasense-dh-gps02 and permasense-dh-gps03 to upload script

--- gsn/tools/backlog/upload_backlog_productive.sh
+++ gsn/tools/backlog/upload_backlog_productive.sh
@@ -13,25 +13,41 @@ done
 
 echo "uploading $*"
 
+echo ""
 echo "to permasense-dh-bs01:"
 scp $* root@permasense-dh-bs01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-jj-bs01:"
 scp $* root@permasense-jj-bs01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-mh-bs01:"
 scp $* root@permasense-mh-bs01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-rd-bs01:"
 scp $* root@permasense-rd-bs01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-thur-bs01:"
 scp $* root@permasense-thur-bs01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-dh-gps01:"
 scp $* root@permasense-dh-gps01:$BACKLOG_PYTHON_DIR
 
+echo ""
+echo "to permasense-dh-gps02:"
+scp $* root@permasense-dh-gps02:$BACKLOG_PYTHON_DIR
+
+echo ""
+echo "to permasense-dh-gps03:"
+scp $* root@permasense-dh-gps03:$BACKLOG_PYTHON_DIR
+
+echo ""
 echo "to permasense-mh-cam01:"
 scp $* root@permasense-mh-cam01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "finished uploading"
--- gsn/tools/backlog/upload_backlog_test.sh
+++ gsn/tools/backlog/upload_backlog_test.sh
@@ -13,34 +13,45 @@ done
 
 echo "uploading $*"
 
+echo ""
 echo "to permasense-etzg-bs01:"
 scp $* root@permasense-etzg-bs01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-etzl-bs01:"
 scp $* root@permasense-etzl-bs01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-etz-gps01:"
 scp $* root@permasense-etz-gps01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-etz-gps02:"
 scp $* root@permasense-etz-gps02:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-etz-gps03:"
 scp $* root@permasense-etz-gps03:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-etz-gps04:"
 scp $* root@permasense-etz-gps04:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-etz-gps05:"
 scp $* root@permasense-etz-gps05:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-etz-gps06:"
 scp $* root@permasense-etz-gps06:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to permasense-etz-cam01:"
 scp $* root@permasense-etz-cam01:$BACKLOG_PYTHON_DIR
 
+echo ""
 echo "to pc-5225:"
 scp $* tgsell@pc-5225:/home/tgsell/backlog/python/
 
+echo ""
 echo "finished uploading"
