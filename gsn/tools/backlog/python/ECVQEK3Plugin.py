061a61c2c8c9012684f5a9bbe7b245d9b5d330ed
dhasenfratz <dhasenfratz@users.sourceforge.net>
2011-05-26 12:55:42 +0000

    opensense backlog python scripts

---
+++ gsn/tools/backlog/python/ECVQEK3Plugin.py
@@ -0,0 +1,65 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__      = "David Hasenfratz <hasenfratz@tik.ee.ethz.ch>"
+__copyright__   = "Copyright 2011, ETH Zurich, Switzerland, David Hasenfratz"
+__license__     = "GPL"
+__version__     = "$Revision$"
+__date__        = "$Date$"
+__id__          = "$Id$"
+
+import BackLogMessage
+from AbstractPlugin import AbstractPluginClass
+
+import ECVQEK3Driver
+
+DEFAULT_BACKLOG = True
+
+# Reads messages from the ECVQEK3Driver and sends them to GSN
+class ECVQEK3PluginClass(AbstractPluginClass):
+    
+    def __init__(self, parent, config):
+	    
+        AbstractPluginClass.__init__(self, parent, config, DEFAULT_BACKLOG)
+        self._timer = None
+        self._stopped = False
+        self._interval = None
+
+        self.info('Init ECVQEK3Plugin...')
+
+        self._deviceStr = self.getOptionValue('ecvqek3_device')
+        self.ecvqek3 = ECVQEK3Driver.ECVQEK3Driver([self._deviceStr])
+
+        self.info("Done init")
+
+    def getMsgType(self):
+        return BackLogMessage.ECVQEK3_MESSAGE_TYPE
+
+    def isBusy(self):
+        return False
+
+    def action(self, parameters):
+
+        self.info('ECVQEK3Plugin started...')
+	
+        # Read message
+        msg = ''
+        countRead = 0
+        readSuccessFlag = 0
+        while len(msg) != 69 and countRead < 10:
+            msg = self.ecvqek3._read()
+            if str(msg) != 'None' and msg != '[NAK]' and msg.find('EK3 ECM') >= 0:
+                self.info(msg)
+                readSuccessFlag = 1
+                break
+            else:
+                countRead += 1
+                
+        if readSuccessFlag == 1:
+            self.processMsg(self.getTimeStamp(), [msg])
+            self.info('ECVQEK3 reading done')
+        else:
+            self.error('ECVQEK3 reading failed')
+
+    def stop(self):
+        self._stopped = True
+#        self.info('stopped')
---
+++ gsn/tools/backlog/python/GPSPluginNAV.py
@@ -0,0 +1,86 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__      = "David Hasenfratz <hasenfratz@tik.ee.ethz.ch>"
+__copyright__   = "Copyright 2011, ETH Zurich, Switzerland, David Hasenfratz"
+__license__     = "GPL"
+__version__     = "$Revision: 2925 $"
+__date__        = "$Date: 2011-03-25 08:50:27 +0100 (Fri, 25 Mar 2011) $"
+__id__          = "$Id: GPSPluginNAV.py 2925 2011-03-25 07:50:27Z dhasenfratz $"
+
+'''
+backlog imports
+'''
+import BackLogMessage
+from AbstractPlugin import AbstractPluginClass
+
+'''
+stdlib imports
+'''
+import struct
+import copy
+import time
+import GPSDriverNAV
+'''
+defines
+'''
+
+DEFAULT_BACKLOG = True
+
+NAV_TYPE = 1
+RAW_TYPE = 2
+
+#Reads raw or nav GPS messages from a u-blox device and sends them to GSN
+
+class GPSPluginNAVClass(AbstractPluginClass):
+    
+    def __init__(self, parent, config):
+	    
+        AbstractPluginClass.__init__(self, parent, config, DEFAULT_BACKLOG)
+        self._timer = None
+        self._stopped = False
+        self._interval = 1
+        
+        self.info('Init GPSPluginNAV...')
+        
+        # serial port timeout
+        self._serialTimeout = 1
+        
+        self._deviceStr = self.getOptionValue('gps_device')
+        self.gps = GPSDriverNAV.GPSDriverNAV([self._deviceStr, self._interval])
+        
+        self.info("Done init")
+
+    def getMsgType(self):
+        return BackLogMessage.GPS_MESSAGE_TYPE
+
+    def isBusy(self):
+        return False
+
+    def action(self, parameters):
+
+        self.info('GPSPluginNAV started...')
+        
+        # Read GPS message
+        gpsMsg = self.gps._read("")
+        # Parse message
+        dataPackage = self._parseNavMsg(gpsMsg)
+        self.processMsg(self.getTimeStamp(), dataPackage)
+        
+        self.info('GPS reading done')
+
+    def stop(self):
+        self._stopped = True
+        self.info('stopped')
+
+    def _parseNavMsg(self, msg):
+        if (msg):
+            dataPackage = [NAV_TYPE]
+            for i in range(0, len(msg[2])):
+                dataPackage += [msg[2][i]]
+
+            self.info(dataPackage)
+            return dataPackage
+
+        else:
+            self.info("WARNING: MSG packet was empty!")
+        
