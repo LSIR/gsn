4812316dfded4298e429cb8711cfdf3d25315448
dhasenfratz <dhasenfratz@users.sourceforge.net>
2011-05-27 13:18:38 +0000

    gps nav plugin

---
+++ gsn/src/gsn/wrappers/backlog/plugins/GPSNAVPlugin.java
@@ -0,0 +1,63 @@
+package gsn.wrappers.backlog.plugins;
+
+import java.io.Serializable;
+
+import org.apache.log4j.Logger;
+
+import gsn.beans.DataField;
+
+
+public class GPSNAVPlugin extends AbstractPlugin {
+
+	private DataField[] dataField = {	new DataField("TIMESTAMP", "BIGINT"),
+						new DataField("GENERATION_TIME", "BIGINT"),
+						new DataField("DEVICE_ID", "INTEGER"),
+						new DataField("UTC_RAW_DATA", "VARCHAR(32)"),
+						new DataField("LAT_RAW_DATA", "VARCHAR(32)"),
+						new DataField("LAT_N_RAW_DATA", "VARCHAR(32)"),
+						new DataField("LONG_RAW_DATA", "VARCHAR(32)"),
+						new DataField("LONG_N_RAW_DATA", "VARCHAR(32)"),
+						new DataField("QUAL_RAW_DATA", "VARCHAR(32)"),
+						new DataField("SAT_RAW_DATA", "VARCHAR(32)"),
+						new DataField("HDOP_RAW_DATA", "VARCHAR(32)"),
+						new DataField("GEO_HEIGHT_RAW_DATA", "VARCHAR(32)"),
+						new DataField("GEO_HEIGHT_U_RAW_DATA", "VARCHAR(32)"),
+						new DataField("GEO_SEP_RAW_DATA", "VARCHAR(32)"),
+						new DataField("GEO_SEP_U_RAW_DATA", "VARCHAR(32)")};
+
+	private final transient Logger logger = Logger.getLogger( GPSNAVPlugin.class );
+
+	@Override
+	public byte getMessageType() {
+		return gsn.wrappers.backlog.BackLogMessage.GPS_NAV_MESSAGE_TYPE;
+	}
+
+
+	@Override
+	public String getPluginName() {
+		return "GPSNAVPlugin";
+	}
+
+	@Override
+	public DataField[] getOutputFormat() {
+		return dataField;
+	}
+
+	@Override
+	public boolean messageReceived(int deviceId, long timestamp, Serializable[] data) {
+
+		if (data.length != 12) {
+			logger.error("The message with timestamp >" + timestamp + "< seems unparsable.(length: " + data.length + ")");
+			ackMessage(timestamp, super.priority);
+			return true;
+		}
+
+
+		if( dataProcessed(System.currentTimeMillis(), new Serializable[] {timestamp, timestamp, deviceId, data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12]}) )
+			ackMessage(timestamp, super.priority);
+		else
+			logger.warn("The message with timestamp >" + timestamp + "< could not be stored in the database.");
+
+		return true;
+	}
+}
