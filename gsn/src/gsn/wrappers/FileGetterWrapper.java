e96bdb8539c7b3a321c39be5c0c0bb2c47c7ae40
tgsell <tgsell@ethz.ch>
2011-06-20 13:18:05 +0000

    device id will be extracted from filename

--- gsn/src/gsn/wrappers/FileGetterWrapper.java
+++ gsn/src/gsn/wrappers/FileGetterWrapper.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.naming.OperationNotSupportedException;
@@ -25,6 +26,7 @@ public class FileGetterWrapper extends AbstractWrapper {
 	private File deploymentBinaryDir = null;
 	private String subdirectoryName = null;
 	private Pattern filenamePattern = null;
+	private boolean deviceIdFromFilename;
 	final static private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd_HHmmss");
 
 	@Override
@@ -75,6 +77,14 @@ public class FileGetterWrapper extends AbstractWrapper {
 		if (regex != null) {
 			filenamePattern = Pattern.compile(regex);
 			logger.info("filename has to match regular expression: " + regex);
+			
+			deviceIdFromFilename = Boolean.parseBoolean(getActiveAddressBean().getPredicateValue("deviceid-from-filename"));
+			if (deviceIdFromFilename)
+				logger.info("device id will be extracted from filename using the first group from regular expresion: " + regex);
+		}
+		else if (Boolean.parseBoolean(getActiveAddressBean().getPredicateValue("deviceid-from-filename"))) {
+			logger.error("deviceid-from-filename predicate key can only be used together with filename-regex");
+			return false;
 		}
 		
 		return true;
@@ -83,81 +93,80 @@ public class FileGetterWrapper extends AbstractWrapper {
 	@Override
 	public boolean sendToWrapper ( String action , String [ ] paramNames , Object [ ] paramValues ) throws OperationNotSupportedException {
 		if( action.compareToIgnoreCase("logger_file") == 0 ) {
-			long gentime = System.currentTimeMillis();
-			String deviceid = null;
-			FileItem inputFileItem = null;
-			if( paramNames.length != 2 ) {
-				logger.error("logger_file action must have two parameter names: 'device_id' and 'file'");
-				return false;
-			}
-			if( paramValues.length != 2 ) {
-				logger.error("logger_file action must have two parameter values");
-				return false;
-			}
-			
-			for( int i=0; i<2; i++ ) {
-				try {
+			try {
+				long gentime = System.currentTimeMillis();
+				String deviceid = null;
+				FileItem inputFileItem = null;
+				
+				for( int i=0; i<paramNames.length; i++ ) {
 					String tmp = paramNames[i];
 					if( tmp.compareToIgnoreCase("device_id") == 0 )
 						deviceid = (String) paramValues[i];
 					else if( tmp.compareToIgnoreCase("file") == 0 )
 						inputFileItem = (FileItem) paramValues[i];
-				} catch(Exception e) {
-					logger.error("Could not interprete logger_file arguments: " + e.getMessage());
+				}
+				
+				if (filenamePattern != null) {
+					Matcher m = filenamePattern.matcher(inputFileItem.getName());
+					if (!m.matches()) {
+						logger.error("filename " + inputFileItem.getName() + " does not match regular expression " + filenamePattern.toString());
+						return false;
+					}
+	
+					if (deviceIdFromFilename)
+						deviceid = m.group(1);
+				}
+				
+				if (deviceid.equals("")) {
+					logger.error("device_id argument has to be an integer between 0 and 65535");
 					return false;
 				}
-			}
-			
-			if (filenamePattern != null && !filenamePattern.matcher(inputFileItem.getName()).matches()) {
-				logger.error("filename " + inputFileItem.getName() + " does not match regular expression " + filenamePattern.toString());
-				return false;
-			}
-
-			if (deviceid.equals("")) {
-				logger.error("device_id argument has to be an integer between 0 and 65535");
-				return false;
-			}
-			Integer id;
-			try {
-				id = Integer.parseInt(deviceid);
-			} catch(Exception e) {
-				logger.error("Could not interprete device_id argument: " + e.getMessage(), e);
-				return false;
-			}
-			if (id < 0 || id > 65534) {
-				logger.error("device_id argument has to be an integer between 0 and 65535");
-				return false;
-			}
-			
-			File storageDir = new File(new File(deploymentBinaryDir, deviceid), subdirectoryName);
-			if (!storageDir.exists()) {
-		    	if (!storageDir.mkdirs()) {
-		    		logger.error("could not mkdir >" + storageDir + "<");
-		    		return false;
-				}
-		    	else
-		    		logger.info("created new storage directory >" + storageDir + "<");
-			}
-			
-			if (inputFileItem.getSize() <= 0) {
-				logger.warn("uploaded file is empty => skip it");
-				return false;
-			}
 				
-			int pos = inputFileItem.getName().lastIndexOf('.');
-			String suffix = "";
-			if (pos > 0 && pos < inputFileItem.getName().length() - 1)
-				suffix = inputFileItem.getName().substring(pos);
-			String filename = format.format(new java.util.Date(gentime))+suffix;
-			File outputFile = new File(storageDir, filename);
-			try {
-				inputFileItem.write(outputFile);
-			} catch (Exception e) {
-				logger.error(e.getMessage());
+				Integer id;
+				try {
+					id = Integer.parseInt(deviceid);
+				} catch(Exception e) {
+					logger.error("Could not interprete device_id argument (" + deviceid +") as integer");
+					return false;
+				}
+				if (id < 0 || id > 65534) {
+					logger.error("device_id argument has to be an integer between 0 and 65535");
+					return false;
+				}
+				
+				File storageDir = new File(new File(deploymentBinaryDir, id.toString()), subdirectoryName);
+				if (!storageDir.exists()) {
+			    	if (!storageDir.mkdirs()) {
+			    		logger.error("could not mkdir >" + storageDir + "<");
+			    		return false;
+					}
+			    	else
+			    		logger.info("created new storage directory >" + storageDir + "<");
+				}
+				
+				if (inputFileItem.getSize() <= 0) {
+					logger.warn("uploaded file is empty => skip it");
+					return false;
+				}
+					
+				int pos = inputFileItem.getName().lastIndexOf('.');
+				String suffix = "";
+				if (pos > 0 && pos < inputFileItem.getName().length() - 1)
+					suffix = inputFileItem.getName().substring(pos);
+				String filename = format.format(new java.util.Date(gentime))+suffix;
+				File outputFile = new File(storageDir, filename);
+				try {
+					inputFileItem.write(outputFile);
+				} catch (Exception e) {
+					logger.error(e.getMessage());
+					return false;
+				}
+				
+				return postStreamElement(new Serializable[]{id, gentime, subdirectoryName + "/" + filename});
+			} catch(Exception e) {
+				logger.error(e.getMessage(), e);
 				return false;
 			}
-			
-			return postStreamElement(new Serializable[]{Integer.parseInt(deviceid), gentime, subdirectoryName + "/" + filename});
 		}
 		else {
 			logger.warn("action >" + action + "< not supported");
