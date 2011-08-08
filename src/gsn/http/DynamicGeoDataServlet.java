0655a7d443f9de325345b7724132af6e4de0b0d9
sofiane sarni <sofiane.sarni@epfl.ch>
2011-08-08 23:14:06 +0000

    added basic query for latest data readings

--- src/gsn/http/DynamicGeoDataServlet.java
+++ src/gsn/http/DynamicGeoDataServlet.java
@@ -1,5 +1,7 @@
 package gsn.http;
 
+import com.vividsolutions.jts.io.ParseException;
+import gsn.Main;
 import gsn.Mappings;
 import gsn.beans.VSensorConfig;
 import org.apache.log4j.Logger;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
@@ -17,6 +20,8 @@ import java.util.Vector;
 public class DynamicGeoDataServlet extends HttpServlet {
 
     private static transient Logger logger = Logger.getLogger(DynamicGeoDataServlet.class);
+    private static final String SEPARATOR = ",";
+    private static final String NEWLINE = "\n";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
@@ -33,15 +38,18 @@ public class DynamicGeoDataServlet extends HttpServlet {
         String allSensorsAsString = getAllSensorsAsString();
         List allSensors = getAllSensors();
         StringBuilder sqlQueryStr = new StringBuilder();
-        sqlQueryStr.append("select ");
         for (int i = 0; i < allSensors.size(); i++) {
-            sqlQueryStr.append(allSensors.get(i))
-                    .append(" as name, ")
+            sqlQueryStr.append("select '" + allSensors.get(i) + "'")
+                    .append(" as name, timed, from_unixtime(timed/1000), ")
                     .append(field)
+                    .append(", latitude, longitude, altitude ")
                     .append(" from ")
                     .append(allSensors.get(i))
-                    .append(" where timed = ")
-                    .append(timed);
+                    .append(" where timed = ( select max(timed) from ")
+                    .append(allSensors.get(i))
+                    .append(" )")
+            //.append(timed)
+            ;
             if (i < allSensors.size() - 1)
                 sqlQueryStr.append("\n union \n");
         }
@@ -57,7 +65,9 @@ public class DynamicGeoDataServlet extends HttpServlet {
                 .append("\n")
                 .append("all_sensors = " + allSensorsAsString)
                 .append("\n")
-                .append(sqlQueryStr);
+                .append(sqlQueryStr)
+                .append("\n##############\n")
+                .append(executeQuery(sqlQueryStr.toString()));
         logger.warn(sb.toString());
         response.getWriter().write(sb.toString());
     }
@@ -101,5 +111,58 @@ public class DynamicGeoDataServlet extends HttpServlet {
         if (matchingSensors.length() > 0)
             matchingSensors.setLength(matchingSensors.length() - 1); // remove the last SEPARATOR
         return matchingSensors.toString();
+    }
+
+    public static String executeQuery(String query) {
+
+
+        StringBuilder sb = new StringBuilder();
+        Connection connection = null;
+
+        try {
+            connection = Main.getDefaultStorage().getConnection();
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet results = statement.executeQuery(query);
+            ResultSetMetaData metaData;    // Additional information about the results
+            int numCols, numRows;          // How many rows and columns in the table
+            metaData = results.getMetaData();       // Get metadata on them
+            numCols = metaData.getColumnCount();    // How many columns?
+            results.last();                         // Move to last row
+            numRows = results.getRow();             // How many rows?
+
+            String s;
+
+            sb.append("# ");
+
+            for (int col = 0; col < numCols; col++) {
+                sb.append(metaData.getColumnLabel(col + 1));
+                if (col < numCols - 1)
+                    sb.append(SEPARATOR);
+            }
+            sb.append(NEWLINE);
+
+            for (int row = 0; row < numRows; row++) {
+                results.absolute(row + 1);                // Go to the specified row
+                for (int col = 0; col < numCols; col++) {
+                    Object o = results.getObject(col + 1); // Get value of the column
+                    //logger.warn(row + " , "+col+" : "+ o.toString());
+                    if (o == null)
+                        s = "null";
+                    else
+                        s = o.toString();
+                    if (col < numCols - 1)
+                        sb.append(s).append(SEPARATOR);
+                    else
+                        sb.append(s);
+                }
+                sb.append(NEWLINE);
+            }
+        } catch (SQLException e) {
+            sb.append("ERROR in execution of query: " + e.getMessage());
+        } finally {
+            Main.getDefaultStorage().close(connection);
+        }
+
+        return sb.toString();
     }
 }
