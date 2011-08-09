ca4feb1335b3f1ff5daa0db9b54231973acf3b5a
sofiane sarni <sofiane.sarni@epfl.ch>
2011-08-09 13:39:08 +0000

    added support for authentication using username and password parameters

--- src/gsn/http/DynamicGeoDataServlet.java
+++ src/gsn/http/DynamicGeoDataServlet.java
@@ -1,9 +1,9 @@
 package gsn.http;
 
-import com.vividsolutions.jts.io.ParseException;
 import gsn.Main;
 import gsn.Mappings;
 import gsn.beans.VSensorConfig;
+import gsn.http.ac.UserUtils;
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
@@ -34,23 +34,44 @@ public class DynamicGeoDataServlet extends HttpServlet {
         String field = HttpRequestUtils.getStringParameter("field", null, request);
         String timed = HttpRequestUtils.getStringParameter("timed", null, request);
         String query = HttpRequestUtils.getStringParameter("query", "value", request);
+        String username = HttpRequestUtils.getStringParameter("username", null, request);
+        String password = HttpRequestUtils.getStringParameter("password", null, request);
+
+        List<String> allowedSensors = new Vector<String>();
+
+        if (Main.getContainerConfig().isAcEnabled()) {
+            if (username != null && password != null) {
+                if (UserUtils.allowUserToLogin(username, password) == null) {
+                    response.getWriter().write("ERROR: incorrect login for user '" + username + "'. Check your credentials.");
+                    return;
+                } else { // user authenticated correctly
+                    allowedSensors = UserUtils.getAllowedVirtualSensorsForUser(username, password, getAllSensors());
+                }
+
+            } else { // username or password is null
+                response.getWriter().write("ERROR: username and password required.");
+                return;
+            }
+        } else { // No access control
+            allowedSensors = getAllSensors();
+        }
+
         StringBuilder sb = new StringBuilder();
-        String allSensorsAsString = getAllSensorsAsString();
-        List allSensors = getAllSensors();
+
         StringBuilder sqlQueryStr = new StringBuilder();
-        for (int i = 0; i < allSensors.size(); i++) {
-            sqlQueryStr.append("select '" + allSensors.get(i) + "'")
+        for (int i = 0; i < allowedSensors.size(); i++) {
+            sqlQueryStr.append("select '" + allowedSensors.get(i) + "'")
                     .append(" as name, timed, from_unixtime(timed/1000), ")
                     .append(field)
                     .append(", latitude, longitude, altitude ")
                     .append(" from ")
-                    .append(allSensors.get(i))
+                    .append(allowedSensors.get(i))
                     .append(" where timed = ( select max(timed) from ")
-                    .append(allSensors.get(i))
+                    .append(allowedSensors.get(i))
                     .append(" )")
             //.append(timed)
             ;
-            if (i < allSensors.size() - 1)
+            if (i < allowedSensors.size() - 1)
                 sqlQueryStr.append("\n union \n");
         }
 
@@ -63,7 +84,7 @@ public class DynamicGeoDataServlet extends HttpServlet {
                 .append("\n")
                 .append("query = " + query)
                 .append("\n")
-                .append("all_sensors = " + allSensorsAsString)
+                .append("all_sensors = " + sensorsToString(allowedSensors))
                 .append("\n")
                 .append(sqlQueryStr)
                 .append("\n##############\n")
@@ -79,9 +100,9 @@ public class DynamicGeoDataServlet extends HttpServlet {
 
         while (iter.hasNext()) {
             VSensorConfig sensorConfig = (VSensorConfig) iter.next();
-            Double longitude = sensorConfig.getLongitude();
-            Double latitude = sensorConfig.getLatitude();
-            Double altitude = sensorConfig.getAltitude();
+            //Double longitude = sensorConfig.getLongitude();
+            //Double latitude = sensorConfig.getLatitude();
+            //Double altitude = sensorConfig.getAltitude();
             String sensor = sensorConfig.getName();
             sensors.add(sensor);
         }
@@ -89,32 +110,20 @@ public class DynamicGeoDataServlet extends HttpServlet {
         return sensors;
     }
 
-    public String getAllSensorsAsString() {
-        StringBuilder matchingSensors = new StringBuilder();
-
-        Iterator iter = Mappings.getAllVSensorConfigs();
-        List<String> sensors = new Vector<String>();
-
-        while (iter.hasNext()) {
-            VSensorConfig sensorConfig = (VSensorConfig) iter.next();
-            Double longitude = sensorConfig.getLongitude();
-            Double latitude = sensorConfig.getLatitude();
-            Double altitude = sensorConfig.getAltitude();
-            String sensor = sensorConfig.getName();
-            sensors.add(sensor);
+    public String sensorsToString(List<String> sensors) {
+        StringBuilder sensorsAsString = new StringBuilder();
+        for (String sensor : sensors) {
+            sensorsAsString.append(sensor);
+            sensorsAsString.append(SEPARATOR);
         }
+        if (sensorsAsString.length() > 0)
+            sensorsAsString.setLength(sensorsAsString.length() - 1);  // remove the last SEPARATOR
 
-        for (String vsName : sensors) {
-            matchingSensors.append(vsName);
-            matchingSensors.append(GetSensorDataWithGeo.SEPARATOR);
-        }
-        if (matchingSensors.length() > 0)
-            matchingSensors.setLength(matchingSensors.length() - 1); // remove the last SEPARATOR
-        return matchingSensors.toString();
+        return sensorsAsString.toString();
     }
 
-    public static String executeQuery(String query) {
 
+    public static String executeQuery(String query) {
 
         StringBuilder sb = new StringBuilder();
         Connection connection = null;
