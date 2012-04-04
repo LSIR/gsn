6a6312587e361c29b6ee5cae6f2f4b0027a5f4ee
sofiane sarni <sofiane.sarni@epfl.ch>
2012-04-04 15:55:49 +0000

    added missing function

--- src/gsn/utils/geo/GridTools.java
+++ src/gsn/utils/geo/GridTools.java
@@ -1,10 +1,13 @@
 package gsn.utils.geo;
 
+import gsn.Main;
+import gsn.beans.DataTypes;
 import org.apache.log4j.Logger;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.sql.*;
 
 public class GridTools {
 
@@ -80,5 +83,68 @@ public class GridTools {
         }
 
         return deserial;
+    }
+
+    public static String executeQueryForGridAsString(String query) {
+
+        Connection connection = null;
+        StringBuilder sb = new StringBuilder();
+        ResultSet results = null;
+
+        try {
+            connection = Main.getDefaultStorage().getConnection();
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            results = statement.executeQuery(query);
+            ResultSetMetaData metaData;    // Additional information about the results
+            int numCols, numRows;          // How many rows and columns in the table
+            metaData = results.getMetaData();       // Get metadata on them
+            numCols = metaData.getColumnCount();    // How many columns?
+            results.last();                         // Move to last row
+            numRows = results.getRow();             // How many rows?
+
+            String s;
+
+            // headers
+            sb.append("# Query: " + query + "\n");
+            sb.append("# ");
+
+            byte typ[] = new byte[numCols];
+            String columnLabel[] = new String[numCols];
+
+            for (int col = 0; col < numCols; col++) {
+                columnLabel[col] = metaData.getColumnLabel(col + 1);
+                typ[col] = Main.getDefaultStorage().convertLocalTypeToGSN(metaData.getColumnType(col + 1));
+            }
+
+            for (int row = 0; row < numRows; row++) {
+                results.absolute(row + 1);                // Go to the specified row
+                for (int col = 0; col < numCols; col++) {
+                    Object o = results.getObject(col + 1); // Get value of the column
+                    if (o == null)
+                        s = "null";
+                    else
+                        s = o.toString();
+                    if (typ[col] == DataTypes.BINARY) {
+                        byte[] bin = (byte[]) o;
+                        sb.append(GridTools.deSerializeToString(bin));
+                    } else {
+                        sb.append(columnLabel[col] + " " + s + "\n");
+                    }
+                }
+                sb.append("\n");
+            }
+        } catch (SQLException e) {
+            sb.append("ERROR in execution of query: " + e.getMessage());
+        } finally {
+            if (results != null)
+                try {
+                    results.close();
+                } catch (SQLException e) {
+                    logger.warn(e.getMessage(), e);
+                }
+            Main.getDefaultStorage().close(connection);
+        }
+
+        return sb.toString();
     }
 }
