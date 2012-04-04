79c64cf63c6697e8fc5e5527862a31d17577b7ce
sofiane sarni <sofiane.sarni@epfl.ch>
2012-04-04 16:13:42 +0000

    added function to return grid as json object

--- src/gsn/utils/geo/GridTools.java
+++ src/gsn/utils/geo/GridTools.java
@@ -61,7 +61,6 @@ public class GridTools {
 
             in = new ObjectInputStream(bis);
 
-
             deserial = (Double[][]) in.readObject();
             in.close();
 
@@ -87,6 +86,70 @@ public class GridTools {
 
     public static String executeQueryForGridAsString(String query) {
 
+        Connection connection = null;
+        StringBuilder sb = new StringBuilder();
+        ResultSet results = null;
+
+        try {
+            connection = Main.getDefaultStorage().getConnection();
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            results = statement.executeQuery(query);
+            ResultSetMetaData metaData;    // Additional information about the results
+            int numCols, numRows;          // How many rows and columns in the table
+            metaData = results.getMetaData();       // Get metadata on them
+            numCols = metaData.getColumnCount();    // How many columns?
+            results.last();                         // Move to last row
+            numRows = results.getRow();             // How many rows?
+
+            String s;
+
+            // headers
+            sb.append("# Query: " + query + "\n");
+            sb.append("# ");
+
+            byte typ[] = new byte[numCols];
+            String columnLabel[] = new String[numCols];
+
+            for (int col = 0; col < numCols; col++) {
+                columnLabel[col] = metaData.getColumnLabel(col + 1);
+                typ[col] = Main.getDefaultStorage().convertLocalTypeToGSN(metaData.getColumnType(col + 1));
+            }
+
+            for (int row = 0; row < numRows; row++) {
+                results.absolute(row + 1);                // Go to the specified row
+                for (int col = 0; col < numCols; col++) {
+                    Object o = results.getObject(col + 1); // Get value of the column
+                    if (o == null)
+                        s = "null";
+                    else
+                        s = o.toString();
+                    if (typ[col] == DataTypes.BINARY) {
+                        byte[] bin = (byte[]) o;
+                        sb.append(GridTools.deSerializeToString(bin));
+                    } else {
+                        sb.append(columnLabel[col] + " " + s + "\n");
+                    }
+                }
+                sb.append("\n");
+            }
+        } catch (SQLException e) {
+            sb.append("ERROR in execution of query: " + e.getMessage());
+        } finally {
+            if (results != null)
+                try {
+                    results.close();
+                } catch (SQLException e) {
+                    logger.warn(e.getMessage(), e);
+                }
+            Main.getDefaultStorage().close(connection);
+        }
+
+        return sb.toString();
+    }
+
+    public static String executeQueryForGridAsJSON(String sensor, long timestamp) {
+
+        String query = new StringBuilder("select * from ").append(sensor).append(" where timed=").append(timestamp).toString();
         Connection connection = null;
         StringBuilder sb = new StringBuilder();
         ResultSet results = null;
