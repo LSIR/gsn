aa146cde4784a22b45a181ef7bdf866416209f7e
sofiane sarni <sofiane.sarni@epfl.ch>
2012-04-04 15:17:37 +0000

    Added method for deserialization to array of doubles

--- src/gsn/utils/geo/GridTools.java
+++ src/gsn/utils/geo/GridTools.java
@@ -10,7 +10,7 @@ public class GridTools {
 
     private static transient Logger logger = Logger.getLogger(GridTools.class);
 
-    public static String deSerialize(byte[] bytes) {
+    public static String deSerializeToString(byte[] bytes) {
 
         StringBuilder sb = new StringBuilder();
 
@@ -43,5 +43,42 @@ public class GridTools {
         }
 
         return sb.toString();
+    }
+
+    /*
+    * deserialization
+    * */
+    public static Double[][] deSerialize(byte[] bytes) {
+
+        Double deserial[][] = new Double[0][];
+
+        try {
+            ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+            ObjectInputStream in = null;
+
+            in = new ObjectInputStream(bis);
+
+
+            deserial = (Double[][]) in.readObject();
+            in.close();
+
+            logger.debug("deserial.length" + deserial.length);
+            logger.debug("deserial[0].length" + deserial[0].length);
+
+            for (int i = 0; i < deserial.length; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < deserial[0].length; j++) {
+                    sb.append(deserial[i][j]).append(" ");
+                }
+                logger.debug(sb.toString());
+            }
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        } catch (ClassNotFoundException e) {
+            logger.error(e.getMessage(), e);
+        }
+
+        return deserial;
     }
 }
