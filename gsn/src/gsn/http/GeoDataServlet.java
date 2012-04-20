6cf52692c0fef839257fb4d818e97a8961cb6a14
tgsell <tgsell@ethz.ch>
2012-04-20 11:44:21 +0000

    apply changeset r2374 - r2479

--- gsn/src/gsn/http/GeoDataServlet.java
+++ gsn/src/gsn/http/GeoDataServlet.java
@@ -11,59 +11,134 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import static gsn.http.GetSensorDataWithGeo.*;
-
+import java.util.Properties;
 
 public class GeoDataServlet extends HttpServlet {
 
     private static transient Logger logger = Logger.getLogger(GeoDataServlet.class);
+    private boolean usePostGIS = true; // by default use JTS
+    private User user = null;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        CheckGISToolkitToUse();
+
         try {
-            User user = null;
+
             if (Main.getContainerConfig().isAcEnabled()) {
                 HttpSession session = request.getSession();
                 user = (User) session.getAttribute("user");
-                response.setHeader("Cache-Control","no-store");
+                response.setHeader("Cache-Control", "no-store");
                 response.setDateHeader("Expires", 0);
-                response.setHeader("Pragma","no-cache");
+                response.setHeader("Pragma", "no-cache");
             }
-            GetSensorDataWithGeo g = GetSensorDataWithGeo.getInstance();
-            g.buildGeoIndex();
+
             String env = HttpRequestUtils.getStringParameter("env", null, request); // e.g. "POLYGON ((0 0, 0 100, 100 100, 100 0, 0 0))";
             String query = HttpRequestUtils.getStringParameter("query", null, request);
 
-            response.getWriter().write("List of all sensors: \n" + getListOfSensors() + "\n");
-            response.getWriter().write("Envelope: " + env + "\n");
+            if (usePostGIS)
+                response.getWriter().write(runPostGIS(env, query));
+            else
+                response.getWriter().write(runJTS(env, query));
 
-            //
-            ArrayList<String> sensors = g.getListOfSensors(env);
-            StringBuilder matchingSensors = new StringBuilder();
-            for (String vsName : sensors) {
-                if ( ! Main.getContainerConfig().isAcEnabled() || (user != null && (user.hasReadAccessRight(vsName) || user.isAdmin()))) {
-                    matchingSensors.append(vsName);
-                    matchingSensors.append(GetSensorDataWithGeo.SEPARATOR);
-                }
-            }
-            matchingSensors.setLength(matchingSensors.length() - 1); // remove the last SEPARATOR
-            //
-            //response.getWriter().write("List of all sensors within envelope: \n" + g.getListOfSensorsAsString(env) + "\n");
-            response.getWriter().write("List of all sensors within envelope: \n" + matchingSensors + "\n");
-
-
-            response.getWriter().write("Query:" + query + "\n");
-            response.getWriter().write("Query result: \n");
-            response.getWriter().write(executeQuery(env, matchingSensors.toString(), query));
         } catch (ParseException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            logger.warn(e.getMessage(), e);
         }
+    }
+
+    /*
+   * Searches within the list of provided sensors
+   * for the ones matching AC credentials for the current user
+   * */
+    public String getMatchingSensors(ArrayList<String> sensors) {
+        StringBuilder matchingSensors = new StringBuilder();
+
+        for (String vsName : sensors) {
+            if (!Main.getContainerConfig().isAcEnabled() || (user != null && (user.hasReadAccessRight(vsName) || user.isAdmin()))) {
+                matchingSensors.append(vsName);
+                matchingSensors.append(GetSensorDataWithGeo.SEPARATOR);
+            }
+        }
+        matchingSensors.setLength(matchingSensors.length() - 1); // remove the last SEPARATOR
+        return matchingSensors.toString();
+    }
+
+    public String runJTS(String env, String query) throws ParseException {
+
+        StringBuilder response = new StringBuilder();
+
+        GetSensorDataWithGeo.buildGeoIndex();
+
+        response.append("List of all sensors: \n" + GetSensorDataWithGeo.getListOfSensors() + "\n");
+        response.append("Envelope: " + env + "\n");
+
+        ArrayList<String> sensors = GetSensorDataWithGeo.getListOfSensors(env);
+        String matchingSensors = getMatchingSensors(sensors);
+
+        response.append("List of all sensors within envelope: \n" + matchingSensors + "\n");
+
+        response.append("Query:" + query + "\n");
+        response.append("Query result: \n");
+        response.append(GetSensorDataWithGeo.executeQuery(env, matchingSensors.toString(), query));
+
+        return response.toString();
+
+    }
+
+    public String runPostGIS(String env, String query) throws ParseException {
+
+        StringBuilder response = new StringBuilder();
+
+        GetSensorDataWithGeoPostGIS.buildGeoIndex();
+
+        response.append("List of all sensors: \n" + GetSensorDataWithGeoPostGIS.getListOfSensors() + "\n");
+        response.append("Envelope: " + env + "\n");
+
+        ArrayList<String> sensors = GetSensorDataWithGeoPostGIS.getListOfSensors(env);
+        String matchingSensors = getMatchingSensors(sensors);
+
+        response.append("List of all sensors within envelope: \n" + matchingSensors + "\n");
+
+        response.append("Query:" + query + "\n");
+        response.append("Query result: \n");
+        response.append(GetSensorDataWithGeoPostGIS.executeQuery(env, matchingSensors.toString(), query));
+
+        return response.toString();
+
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse res) throws ServletException, IOException {
         doGet(request, res);
+    }
+
+    public void CheckGISToolkitToUse() {
+
+        usePostGIS = false; // use JTS by default
+
+        Properties p = new Properties();
+        try {
+            p.load(new FileInputStream(GetSensorDataWithGeoPostGIS.CONF_SPATIAL_PROPERTIES_FILE));
+        }
+        catch (IOException e) {
+            p = null;
+            logger.warn(e.getMessage(), e);
+        }
+
+        if (p != null) {
+            String typeOFGIS = p.getProperty("type");
+            if (typeOFGIS != null)
+                if (typeOFGIS.trim().toLowerCase().equals("postgis")) {
+                    usePostGIS = true;
+                }
+        }
+
+        if (usePostGIS)
+            logger.warn("Using PostGIS");
+        else
+            logger.warn("Using JTS");
     }
 
 
--- gsn/src/gsn/http/ac/MyControllerFilter.java
+++ gsn/src/gsn/http/ac/MyControllerFilter.java
@@ -19,86 +19,82 @@ import java.io.IOException;
  * Time: 7:25:07 PM
  * To change this template use File | Settings | File Templates.
  */
-public class MyControllerFilter implements Filter
-{
-     private FilterConfig config = null;
-     private static transient Logger logger                             = Logger.getLogger( MyControllerFilter.class );
+public class MyControllerFilter implements Filter {
 
-	public void init(FilterConfig config) throws ServletException
-	{
-		this.config = config;
-	}
+    private FilterConfig config = null;
+    private static transient Logger logger = Logger.getLogger(MyControllerFilter.class);
 
-	public void destroy()
-	{
-		config = null;
-	}
+    public void init(FilterConfig config) throws ServletException
+    {
+        this.config = config;
+    }
 
-    public void doFilter(ServletRequest requset, ServletResponse response, FilterChain chain)throws IOException, ServletException
-	{
-            if (requset instanceof HttpServletRequest)
+    public void destroy()
+    {
+        config = null;
+    }
+
+    public void doFilter(ServletRequest requset, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (requset instanceof HttpServletRequest)
+        {
+            HttpServletRequest req = (HttpServletRequest) requset;
+            HttpServletResponse res = (HttpServletResponse) response;
+            HttpSession session = req.getSession();
+            User user = (User) session.getAttribute("user");
+            if (Main.getContainerConfig().isAcEnabled() == false)// do as filter does not exist
             {
-                HttpServletRequest req=(HttpServletRequest)requset;
-                HttpServletResponse res=(HttpServletResponse)response;
-                HttpSession session = req.getSession();
-                User user = (User) session.getAttribute("user");
-                if(Main.getContainerConfig().isAcEnabled()==false)// do as filter does not exist
-                {
-                      chain.doFilter(requset,response);
-                }
-                else
-                {
+                chain.doFilter(requset, response);
+            } else {
 
-                    // bypass if servlet is gsn and request is for ContainerInfoHandler
-                    String rawRequest = req.getParameter ( WebConstants.REQUEST );
-                    int requestType = -1;
-                    if ( rawRequest == null || rawRequest.trim ( ).length ( ) == 0 ) {
-                        requestType = 0;
-                    } else
-                        try {
-                            requestType = Integer.parseInt ( ( String ) rawRequest );
-                        } catch ( Exception e ) {
-                            logger.debug ( e.getMessage ( ) , e );
-                            requestType = -1;
-                        }
-
-                    if ("/gsn".equals(req.getServletPath()) && requestType == 0)
-                    {
-                        chain.doFilter(requset,response);
-                        return ;
+                // bypass if servlet is gsn and request is for ContainerInfoHandler
+                String rawRequest = req.getParameter(WebConstants.REQUEST);
+                int requestType = -1;
+                if (rawRequest == null || rawRequest.trim().length() == 0) {
+                    requestType = 0;
+                } else
+                    try {
+                        requestType = Integer.parseInt((String) rawRequest);
+                    } catch (Exception e) {
+                        logger.debug(e.getMessage(), e);
+                        requestType = -1;
                     }
-                    
-                    //
 
-                    if (user == null)// if user has not already loogged-in
-                    {
-				        if(req.getQueryString()== null) // if there is no query string in uri, we suppose that target is GSN home
-                        {
-                            session.setAttribute("login.target",null);
 
-                        }
-                        else
-                        {
-                           
-                            /* if there is query string, store it as a target, so to go back to it once logged-in */
-                            session.setAttribute("login.target",req.getRequestURL()+"?"+ req.getQueryString());
-                        }
-                        res.setHeader("Cache-Control", "no-cache");
-                        res.setHeader("Pragma", "no-cache");
-                        res.setHeader("Expires", "0");
-
-                        // redirect to login
-                        res.sendRedirect("/gsn/MyLoginHandlerServlet");
-
-				        return;
-			        }
-			        else
-			        {
-                        //if logged-in, go to the target directly 
-				        chain.doFilter(requset,response);
-
-                    }
+                if ( ("/gsn".equals(req.getServletPath()) && requestType == 0)
+                        || ("/multidata".equals(req.getServletPath()))
+                        || ("/field".equals(req.getServletPath())) 
+                        ) {
+                    chain.doFilter(requset, response);
+                    return;
                 }
-    		}
-	}
+
+                //
+
+                if (user == null)// if user has not already loogged-in
+                {
+                    if (req.getQueryString() == null) // if there is no query string in uri, we suppose that target is GSN home
+                    {
+                        session.setAttribute("login.target", null);
+
+                    } else {
+
+                        /* if there is query string, store it as a target, so to go back to it once logged-in */
+                        session.setAttribute("login.target", req.getRequestURL() + "?" + req.getQueryString());
+                    }
+                    res.setHeader("Cache-Control", "no-cache");
+                    res.setHeader("Pragma", "no-cache");
+                    res.setHeader("Expires", "0");
+
+                    // redirect to login
+                    res.sendRedirect("/gsn/MyLoginHandlerServlet");
+
+                    return;
+                } else {
+                    //if logged-in, go to the target directly
+                    chain.doFilter(requset, response);
+
+                }
+            }
+        }
+    }
 }
