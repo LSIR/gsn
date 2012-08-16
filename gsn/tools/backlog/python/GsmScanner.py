e9a43c43dc2c95d32c8550b91c2efbe36fee0661
dhasenfratz <dhasenfratz@users.sourceforge.net>
2012-08-16 15:59:54 +0000

    added revision lines

--- gsn/tools/backlog/python/GsmScanner.py
+++ gsn/tools/backlog/python/GsmScanner.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__      = "David Hasenfratz <hasenfratz@tik.ee.ethz.ch>"
+__copyright__   = "Copyright 2011, ETH Zurich, Switzerland, David Hasenfratz"
+__license__     = "GPL"
+__version__     = "$Revision: 3717 $"
+__date__        = "$Date: 2011-10-13 09:16:45 +0200 (Thu, 13 Oct 2011) $"
+__id__          = "$Id: GPSPluginNAV.py 3717 2011-10-13 07:16:45Z dhasenfratz $"
 
 import Mc75iDriver
 import time
