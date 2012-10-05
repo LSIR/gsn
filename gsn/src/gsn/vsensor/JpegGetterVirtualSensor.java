faa66ca03935b4f0dc132e0d06b74ff591840ee3
tgsell <tgsell@ethz.ch>
2012-10-05 12:38:32 +0000

    automatic image type recognition

--- gsn/src/gsn/vsensor/JpegGetterVirtualSensor.java
+++ gsn/src/gsn/vsensor/JpegGetterVirtualSensor.java
@@ -1,12 +1,14 @@
 package gsn.vsensor;
 
 import java.awt.image.renderable.ParameterBlock;
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.net.URLConnection;
 import java.util.TreeMap;
 
 import javax.imageio.ImageIO;
@@ -23,11 +25,12 @@ import com.sun.media.jai.codec.SeekableStream;
 
 public class JpegGetterVirtualSensor extends BridgeVirtualSensorPermasense {
 	
+	private static final String DEFAULT_NEF_EXTRACTION_TYPE = "thumbnail";
+	
 	private static final String DCRAW = "/home/perma/bin/dcraw-x64/dcraw";
 
 	private static final transient Logger logger = Logger.getLogger(JpegGetterVirtualSensor.class);
 
-	private String filetype = null;
 	private String storage_directory = null;
 	private String nef_extraction_type = null;
 	private String dcraw_flip = null;
@@ -43,18 +46,9 @@ public class JpegGetterVirtualSensor extends BridgeVirtualSensorPermasense {
 		if (storage_directory != null) {
 			storage_directory = new File(storage_directory, deployment).getPath();
 		}
-		filetype = params.get("file_type");
-		if (filetype == null) {
-			logger.error("file_type has to be defined in the virtual sensors xml file");
-			return false;
-		}
-		if (filetype.equalsIgnoreCase("nef")) {
-			nef_extraction_type = params.get("nef_extraction_type");
-			if (nef_extraction_type == null || (!nef_extraction_type.equalsIgnoreCase("thumbnail") && !nef_extraction_type.equalsIgnoreCase("jpeg"))) {
-				logger.error("nef_extraction_type has to be defined in the virtual sensors xml file (valid values are \"thumbnail\" or \"jpeg\")");
-				return false;
-			}
-		}
+		nef_extraction_type = params.get("nef_extraction_type");
+		if (nef_extraction_type == null)
+			nef_extraction_type = DEFAULT_NEF_EXTRACTION_TYPE;
 		
 		dcraw_flip = params.get("dcraw_flip");
 		if (params.get("rotation")!=null)
@@ -87,17 +81,22 @@ public class JpegGetterVirtualSensor extends BridgeVirtualSensorPermasense {
 
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		InputStream is = null;
+		String mimeType = null;
+		try {
+			mimeType = URLConnection.guessContentTypeFromStream(new BufferedInputStream(new FileInputStream(file)));
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
 		
-		if (filetype.equalsIgnoreCase("jpeg")  || filetype.equalsIgnoreCase("jpg")) {
+		if (mimeType != null && (mimeType.equalsIgnoreCase("image/jpeg") || mimeType.equalsIgnoreCase("image/pjpeg"))) {
 			logger.debug("getting jpeg: " + file.getAbsolutePath());
 		    try {
 		    	is = new FileInputStream(file);
 		    } catch (IOException e) {
 		    	logger.error(e.getMessage(), e);
-		    	return;
 		    }
 		}
-		else if (filetype.equalsIgnoreCase("nef")) {
+		else {
 			logger.debug("exctracting jpeg from: " + file.getAbsolutePath());
 			Process p = null;
 			StringBuffer cmd = new StringBuffer(DCRAW + " -c");
@@ -112,39 +111,48 @@ public class JpegGetterVirtualSensor extends BridgeVirtualSensorPermasense {
 				is = p.getInputStream();
 			} catch (IOException e) {
 				logger.error(e.getMessage(), e);
-		    	return;
 			}
 		}
 		
-		SeekableStream s = SeekableStream.wrapInputStream(is, true);
-		RenderedOp image = JAI.create("stream", s);
-		if (rotation!=null) {
-			// rotate
-			logger.debug("rotate " + file.getAbsolutePath() + " by "+rotation);
-			// 	rotation center
-			float centerX = (float)image.getWidth() / 2;
-			float centerY = (float)image.getHeight() / 2;
-			ParameterBlock pb = new ParameterBlock();
-			pb.addSource(image);
-			pb.add(centerX);
-			pb.add(centerY);
-			pb.add((float)(rotation / 180d * Math.PI));
-			pb.add(new javax.media.jai.InterpolationBicubic(10));
-			// create a new, rotated image
-			image = JAI.create("rotate", pb);
+		if (is != null) {
+			SeekableStream s = SeekableStream.wrapInputStream(is, true);
+			RenderedOp image = JAI.create("stream", s);
+			if (rotation!=null) {
+				// rotate
+				logger.debug("rotate " + file.getAbsolutePath() + " by "+rotation);
+				// 	rotation center
+				float centerX = (float)image.getWidth() / 2;
+				float centerY = (float)image.getHeight() / 2;
+				ParameterBlock pb = new ParameterBlock();
+				pb.addSource(image);
+				pb.add(centerX);
+				pb.add(centerY);
+				pb.add((float)(rotation / 180d * Math.PI));
+				pb.add(new javax.media.jai.InterpolationBicubic(10));
+				// create a new, rotated image
+				image = JAI.create("rotate", pb);
+			}
+			try {
+				ImageIO.write(image.getAsBufferedImage(), "jpg", os);
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+		    	return;
+			}
+			
+			data = new StreamElement(data, 
+					new String[]{"jpeg_scaled"},
+					new Byte[]{DataTypes.BINARY},
+					new Serializable[]{os.toByteArray()});
+			
+			try {
+				is.close();
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+			}
+	
+			super.dataAvailable(inputStreamName, data);
 		}
-		try {
-			ImageIO.write(image.getAsBufferedImage(), "jpg", os);
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-	    	return;
-		}
-		
-		data = new StreamElement(data, 
-				new String[]{"jpeg_scaled"},
-				new Byte[]{DataTypes.BINARY},
-				new Serializable[]{os.toByteArray()});
-
-		super.dataAvailable(inputStreamName, data);
+		else
+			logger.error("file " + file.getName() + " could not be forwarded as jpeg");
 	}
 }
