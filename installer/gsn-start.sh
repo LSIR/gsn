7ab6c1fafc24100c4b743a39f9f09ef8c7c22cfd
jpcik <jean.paul.ik@gmail.com>
2014-05-28 13:23:13 +0200

    sh scripts updated to unix lf

--- installer/gsn-start.sh
+++ installer/gsn-start.sh
@@ -3,6 +3,10 @@
 for jarFile in $( ls lib/*jar ); do
      cp=$cp:./$jarFile
 done
-$JAVA_HOME/bin/java -classpath $cp -splash:lib/GSN_green_medium.png -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger -Dorg.mortbay.log.LogFactory.noDiscovery=false gsn.Main 22232 &
+/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java -classpath $cp\
+ -splash:lib/GSN_green_medium.png\
+ -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger\
+ -Dorg.mortbay.log.LogFactory.noDiscovery=false\
+ gsn.Main 22232 &
 
         
