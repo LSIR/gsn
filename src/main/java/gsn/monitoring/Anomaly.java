33030d285ac65f49db9d8f6dc42fb4d818761aa7
muhammadzaheer <smzaheerabbas@gmail.com>
2015-07-29 21:28:55 +0200

    Added group by functionality in anomalies

--- src/main/java/gsn/monitoring/Anomaly.java
+++ src/main/java/gsn/monitoring/Anomaly.java
@@ -4,13 +4,15 @@ import gsn.beans.DataField;
 
 import org.apache.log4j.Logger;
 
-/* Each virtual sensor might have multiple anomalies specified 
+/*  Each virtual sensor could have multiple anomalies
  *  
  *  Each anomaly object consists of:
- *      field on which it is applied, 
- *      the threshold value,
- *      the time interval window over which this anomaly has to be detected
- *      and the function
+ *      function to be applied
+ *      field on which it is applied 
+ *      groupBy clause (Optional)      
+ *      time interval window over which this anomaly has to be detected
+ *      (anomalies would be calculated over entire historical data if an erroneous value is provided)
+ *      threshold value (Optional)
  */
 
 public class Anomaly {
@@ -23,7 +25,7 @@ public class Anomaly {
     // Threshold value
     private String value;
 
-    // Function to be applied (for e.g. positive_outlier, negative_outlier etc.)
+    // Function to be applied (for e.g. positive_outlier, negative_outlier, iqr, unique  etc.)
     private String function;
     
     // Time interval window
@@ -31,28 +33,28 @@ public class Anomaly {
     
     private String timeStr;         // Storing original timeStr to provide feedback in case of error
     
-    private boolean groupBy;
-    private DataField groupByField; 
+    private boolean groupBy;        // If anomaly specs have a groupBy field specified        
+    
+    private DataField groupByField; // groupByField
 
 
     public Anomaly() {}
     
-    public Anomaly (String function, DataField field, String value, DataField groupByField) {
+    public Anomaly (String function, DataField field, String timeStr, DataField groupByField) {
         this.function = function;
         this.field = field;
-        this.value = value;
+        this.timeStr = timeStr;
+        this.time = this.parseTime(timeStr);
         this.groupByField = groupByField;
+        
         if (groupByField != null)
             this.groupBy = true;
-
-        this.time = 0;              // Counter intuitively, Time = 0 means anomaly would be detected over the entire data history
     }
 
-
-    public Anomaly ( String function, DataField field, String value, DataField groupByField ,String timeStr) {
+    public Anomaly ( String function, DataField field, String timeStr, DataField groupByField ,String value) {
         
-        this (function, field, value, groupByField);
-        this.timeStr = timeStr;
+        this (function, field, timeStr, groupByField);
+        this.value= value;
         
 
         this.time = this.parseTime(timeStr);
@@ -65,44 +67,44 @@ public class Anomaly {
     
         
         timeStr = timeStr.trim();
+        
+        if (timeStr.equals("-1"))    //timeStr == -1 means anomaly has to be detected over all the historical data
+            return -1;
+        
         long toReturn = 0;
         
         // timeSpecifer could be m (minutes) , h (hours) or d (days)
         Character timeSpecifier = timeStr.toLowerCase().charAt(timeStr.length()-1);
-        String timeValue = timeStr.substring(0,timeStr.length()-1);
 
-        double rawTime = Double.parseDouble(timeValue);
+        double rawTime = 0;
 
         switch (timeSpecifier) {
         
             case 'm':
                 //Converting minutes to milliseconds
+                rawTime = Double.parseDouble(timeStr.substring(0,timeStr.length()-1)); 
                 toReturn = (long)(rawTime * 60 * 1000);
                 break;
             case 'h':
                 // Converting hours ti milliseconds
+                rawTime = Double.parseDouble(timeStr.substring(0,timeStr.length()-1));
                 toReturn = (long)(rawTime * 60* 60 * 1000);
                 break;
             case 'd':
                 // Converting days to milliseconds
+                rawTime = Double.parseDouble(timeStr.substring(0,timeStr.length()-1));                
                 toReturn = (long)(rawTime * 24 * 60 * 60 * 1000); 
                 break;
             default:
-                toReturn = 0;
+                toReturn = -1;
                 logger.info ("Invalid time specifer in anomaly: " + this.toString() 
-                        + ". Setting time to 0\nValid time format include m, d and h");
-        
-        
+                        + ". Anomaly would be detected over entire data\nValid time specifiers include m, d and h preceeded by a number");
         }
     
         return toReturn; 
     }
-
-
-    
     
     // Getters
-
     public DataField getField () { return field; }
     public String getValue () { return value; }
     public String getFunction () { return function; }
@@ -115,11 +117,13 @@ public class Anomaly {
     
         StringBuilder sb = new StringBuilder();
         
-        sb.append("anomaly." + this.function + "." + this.field.getName() + "=" + this.value);
+        sb.append("anomaly." + this.function + "." + this.field.getName() + "=");
 
         if (this.timeStr != null)
-            sb.append(","+ this.timeStr);
-        
+            sb.append(this.timeStr);
+        if (this.value!= null)
+            sb.append("," + this.value);
+
         if (this.isGroupBy())
            sb.append(",["+ this.groupByField.getName() + "]"); 
         return sb.toString();
